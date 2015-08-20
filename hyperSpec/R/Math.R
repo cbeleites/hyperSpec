c4aa227efbe6479508a763391b99f69227c5fde4
Claudia Beleites <Claudia.Beleites@ipht-jena.de>
2015-08-20 17:05:20 +0200

    bugfix documentation

--- hyperSpec/R/Math.R
+++ hyperSpec/R/Math.R
@@ -1,23 +1,15 @@
 ##' Math Functions for hyperSpec Objects
-##' 
-##' The functions \code{abs}, \code{sign},
-##' 
-##' \code{sqrt},
-##' 
-##' \code{floor}, \code{ceiling}, \code{trunc}, \code{round}, \code{signif},
-##' 
-##' \code{exp}, \code{log}, \code{expm1}, \code{log1p},
-##' 
-##' \code{cos}, \code{sin}, \code{tan}, \code{acos}, \code{asin}, \code{atan},
-##' \code{cosh}, \code{sinh}, \code{tanh}, \code{acosh}, \code{asinh},
-##' \code{atanh},
-##' 
-##' \code{lgamma}, \code{gamma}, \code{digamma}, \code{trigamma},
-##' 
-##' \code{cumsum}, \code{cumprod}, \code{cummax}, \code{cummin}
-##' 
-##' for \code{hyperSpec} objects.
-##' 
+##'
+##' Mathematical functions for hyperSpec Objects.
+##'
+##' The functions \code{abs}, \code{sign}, \code{sqrt}, \code{floor},
+##' \code{ceiling}, \code{trunc}, \code{round}, \code{signif}, \code{exp},
+##' \code{log}, \code{expm1}, \code{log1p}, \code{cos}, \code{sin}, \code{tan},
+##' \code{acos}, \code{asin}, \code{atan}, \code{cosh}, \code{sinh},
+##' \code{tanh}, \code{acosh}, \code{asinh}, \code{atanh}, \code{lgamma},
+##' \code{gamma}, \code{digamma}, \code{trigamma}, \code{cumsum},
+##' \code{cumprod}, \code{cummax}, \code{cummin} for \code{hyperSpec} objects.
+##'
 ##' @aliases  Math Math2 Math,hyperSpec-method Math2,hyperSpec-method abs,hyperSpec-method
 ##' sign,hyperSpec-method sqrt,hyperSpec-method floor,hyperSpec-method ceiling,hyperSpec-method
 ##' trunc,hyperSpec-method round,hyperSpec-method signif,hyperSpec-method exp,hyperSpec-method
@@ -34,7 +26,7 @@
 ##' @rdname math
 ##' @author C. Beleites
 ##' @seealso \code{\link[methods]{S4groupGeneric}} for group generic methods.
-##' 
+##'
 ##' \code{\link[base]{Math}} for the base math functions.
 ##'
 ##' \code{\link[hyperSpec]{Arith}} for arithmetic operators,
@@ -44,16 +36,16 @@
 ##' @keywords methods math
 ##' @export
 ##' @examples
-##' 
-##' 	log (flu) 
-##' 
+##'
+##' 	log (flu)
+##'
 
 setMethod ("Math2", signature (x = "hyperSpec"),
            function (x, digits){
              validObject (x)
-             
+
              x [[]] <- callGeneric (x[[]], digits)
-             
+
              x
            }
            )
@@ -66,7 +58,7 @@ setMethod ("Math2", signature (x = "hyperSpec"),
 setMethod ("log", signature (x = "hyperSpec"),
 					 function (x, base = exp (1), ...){
 					 	validObject (x)
-					 	
+
 					 	x [[]] <-  log (x[[]], base = base)
 					 	x
 					 }
@@ -77,10 +69,10 @@ setMethod ("log", signature (x = "hyperSpec"),
 setMethod ("Math", signature (x = "hyperSpec"),
 					 function (x){
 					 	validObject (x)
-					 	
+
 					 	if (grepl ("^cum", .Generic) || grepl ("gamma$", .Generic))
 					 		warning (paste ("Do you really want to use", .Generic, "on a hyperSpec object?"))
-					 	
+
 					 	x [[]] <- callGeneric (x[[]])
 					 	x
 					 }
