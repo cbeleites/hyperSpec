fb47326a02b250f3c1d9e2d92be2d53483637567
Claudia Beleites <Claudia.Beleites@chemometrix.eu>
2017-02-22 12:53:43 +0100

    fixes to the unit tests for spc.fit.poly*

--- hyperSpec/R/spc.fit.poly.R
+++ hyperSpec/R/spc.fit.poly.R
@@ -1,11 +1,11 @@
 ##' Polynomial Baseline Fitting
 ##' These functions fit polynomal baselines.
-##' 
+##'
 ##' Both functions fit polynomials to be used as baselines. If \code{apply.to}
 ##' is \code{NULL}, a \code{hyperSpec} object with the polynomial coefficients
 ##' is returned, otherwise the polynomials are evaluated on the spectral range
 ##' of \code{apply.to}.
-##' 
+##'
 ##' \code{spc.fit.poly} calculates the least squares fit of order
 ##' \code{poly.order} to the \emph{complete} spectra given in \code{fit.to}.
 ##' Thus \code{fit.to} needs to be cut appropriately.
@@ -18,7 +18,7 @@
 ##'   If \code{NULL}, a \code{hyperSpec} object containing the polynomial
 ##'   coefficients rather than evaluted baselines is returned.
 ##' @param poly.order order of the polynomial to be used
-##' @param offset.wl should the wavelength range be mapped to -> [0, delta wl]? 
+##' @param offset.wl should the wavelength range be mapped to -> [0, delta wl]?
 ##' This enhances numerical stability.
 ##' @return \code{hyperspec} object containing the baselines in the spectra
 ##'   matrix, either as polynomial coefficients or as polynomials evaluted on
@@ -28,13 +28,13 @@
 ##' @keywords manip datagen
 ##' @export
 ##' @examples
-##' 
+##'
 ##' \dontrun{vignette ("baseline", package = "hyperSpec")}
-##' 
+##'
 ##' spc <- chondro [1 : 10]
 ##' baselines <- spc.fit.poly(spc [,, c (625 ~ 640, 1785 ~ 1800)], spc)
 ##' plot(spc - baselines)
-##' 
+##'
 spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = ! (is.null (apply.to))){
   chk.hy (fit.to)
   if (! is.null (apply.to))
@@ -44,29 +44,29 @@ spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = !
   validObject (apply.to)
 
   x <- fit.to@wavelength
-  
+
   if (offset.wl){
     minx <- min (x)
     x <- x - min (x)
   } else {
     minx <- 0
   }
-      
+
   x <- outer(x, 0 : poly.order, "^")             # Vandermonde matrix of x
-  
+
   p <- apply (fit.to, 1, function (y, x){qr.solve (x, y)}, x)
 
   if (is.null (apply.to)){
     colnames (p@data$spc) <- paste ("(x - minx)^", 0 : poly.order, sep="")
-    
+
     list (coef = p, minx = minx)
-    
+
   } else {
     wl <- apply.to@wavelength - minx
 
     x <- outer(wl, 0 : poly.order, "^")             # Vandermonde matrix of x
     apply.to@data$spc <- I (t (apply (p[[]], 1, function (p, x) {x %*% p}, x)))
-    
+
     # .wl(apply.to) <- wl
     # colnames (apply.to@data$spc) <- format (wl, digits = 4)
 
@@ -75,21 +75,28 @@ spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = !
 }
 
 .test (spc.fit.poly) <- function (){
+  context ("spc.fit.poly")
+
   bl.nonorm <- spc.fit.poly (flu, flu, poly.order = 3, offset.wl = FALSE)
+
+  tmp <- flu
+  wl (tmp) <- wl (tmp) + 1e4
 
   # test effect of wavelength axis normalization
   # was issue 1 on github
-  tmp <- flu
-  wl (tmp) <- wl (tmp) + 1e4
-  
-  checkException(spc.fit.poly(tmp, poly.order = 3, offset.wl = FALSE))
-  
-  bl.1e4 <- spc.fit.poly(tmp, tmp, poly.order = 3, offset.wl = TRUE)
-  checkEqualsNumeric (bl.nonorm [[]], bl.1e4 [[]])
+  test_that("un-normalized large wavelengths with higher order polynomials throw error", {
+    expect_error (spc.fit.poly(tmp, poly.order = 3, offset.wl = FALSE))
+  })
+
+  test_that("normalized large wavelengths with higher order polynomials", {
+
+    bl.1e4 <- spc.fit.poly(tmp, tmp, poly.order = 3, offset.wl = TRUE)
+    expect_equal (bl.nonorm [[]], bl.1e4 [[]])
+  })
 }
 
-                                        
-##' 
+
+##'
 ##' \code{spc.fit.poly.below} tries to fit the baseline on appropriate spectral
 ##' ranges of the spectra in \code{fit.to}.  For details, see the
 ##' \code{vignette ("baseline")}.
@@ -99,10 +106,10 @@ spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = !
 ##'   as one value for all the spectra, or for each spectrum separately.
 ##' @export
 ##' @examples
-##' 
+##'
 ##' baselines <- spc.fit.poly.below (spc)
 ##' plot (spc - baselines)
-##' 
+##'
 spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
                                 npts.min = NULL, noise = 0, offset.wl = FALSE){
   chk.hy (fit.to)
@@ -124,14 +131,14 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
     noise <- rep (noise, nrow (fit.to))
 
   x <- fit.to@wavelength
-    
+
   if (offset.wl){
     minx <- min (x)
     x <- x - min (x)
   } else {
     minx <- 0
   }
-  
+
   vdm <- outer(x, 0 : poly.order, "^")
   y <- t (fit.to [[]])
 
@@ -155,7 +162,7 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
     colnames (fit.to@data$spc) <- paste ("(x - minx)^", 0 : poly.order, sep="")
 
     validObject (fit.to)
-    
+
     fit.to
   } else {
     x <- apply.to@wavelength - minx
@@ -168,40 +175,47 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
     #colnames (apply.to@data$spc) <- format (x, digits = 4)
 
     validObject (apply.to)
-    
+
     apply.to
   }
 }
 
 .test (spc.fit.poly.below) <- function (){
-  bl.nonorm <- spc.fit.poly.below (flu, flu, poly.order = 3, offset.wl = FALSE)
-  
-  # test effect of wavelength axis normalization
-  # was issue 1 on github
+  context ("spc.fit.poly.below")
+
+  bl.nonorm <- spc.fit.poly.below (flu, flu, poly.order = 3, offset.wl = FALSE, npts.min = 12)
+
   tmp <- flu
   wl (tmp) <- wl (tmp) + 1e4
-  
-  checkException(spc.fit.poly.below (tmp, poly.order = 3, offset.wl = FALSE))
-  
-  bl.1e4 <- spc.fit.poly.below (tmp, tmp, poly.order = 3, offset.wl = TRUE)
-  checkEqualsNumeric (bl.nonorm [[]], bl.1e4 [[]])
+
+  # test effect of wavelength axis normalization
+  # was issue 1 on github
+  test_that("un-normalized large wavelengths with higher order polynomials throw error", {
+    expect_error (spc.fit.poly.below (tmp, poly.order = 3, offset.wl = FALSE, npts.min = 12))
+  })
+
+  test_that("normalized large wavelengths with higher order polynomials", {
+    bl.1e4 <- spc.fit.poly.below (tmp, tmp, poly.order = 3, offset.wl = TRUE, npts.min = 12)
+    expect_equal (bl.nonorm [[]], bl.1e4 [[]])
+  })
 }
 
 
 
-##' Matrix of binomial coefficients 
-##' 
-##' Needed to (back)transform poly (x +y) -> poly (x) 
-##'  
+##' Matrix of binomial coefficients
+##'
+##' Needed to (back)transform poly (x +y) -> poly (x)
+##'
+##' @noRd
 .binomial.matrix <- function (order){
   b <- diag (order + 1)
   b [1, ] <- 1
-  
+
   for (o in 2 : order){
     print (o)
     print (b [o, (o + 1) : (order + 1)])
   }
-  
-  colnames (b) <- letters [1 : (order + 1)]  
+
+  colnames (b) <- letters [1 : (order + 1)]
   rownames (b) <- paste0("y^", 0 : order)
 }
