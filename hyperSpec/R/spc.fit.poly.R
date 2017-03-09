a54bc38a320489d87bde5e2d55128297d75537ca
Claudia Beleites <Claudia.Beleites@chemometrix.eu>
2017-03-09 12:12:42 +0100

    fix: issue #40 annoying message in spc.fit.poly.below
    - output changed to `message` and is produced iff `debuglevel >= 1L`
    - message now contains number of support points and `noise` for each spectrum
    - `debuglevel == 2L` plots baseline fitting of 1st spectrum only
    - `debuglevel >= 3L` plots baseline fitting of all spectra

--- hyperSpec/R/spc.fit.poly.R
+++ hyperSpec/R/spc.fit.poly.R
@@ -12,7 +12,6 @@
 ##'
 ##' @rdname baselines
 ##' @concept baseline
-##' @aliases spc.fit.poly spc.fit.poly.below
 ##' @param fit.to \code{hyperSpec} object on which the baselines are fitted
 ##' @param apply.to \code{hyperSpec} object on which the baselines are evaluted
 ##'   If \code{NULL}, a \code{hyperSpec} object containing the polynomial
@@ -20,7 +19,6 @@
 ##' @param poly.order order of the polynomial to be used
 ##' @param offset.wl should the wavelength range be mapped to -> [0, delta wl]?
 ##' This enhances numerical stability.
-##' @param debuglevel  see \code{\link[hyperSpec]{options}}
 ##' @return \code{hyperspec} object containing the baselines in the spectra
 ##'   matrix, either as polynomial coefficients or as polynomials evaluted on
 ##'   the spectral range of \code{apply.to}
@@ -36,8 +34,7 @@
 ##' baselines <- spc.fit.poly(spc [,, c (625 ~ 640, 1785 ~ 1800)], spc)
 ##' plot(spc - baselines)
 ##'
-spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = ! (is.null (apply.to)),
-                          debuglevel = hy.getOption("debuglevel")){
+spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = ! (is.null (apply.to))){
   chk.hy (fit.to)
   if (! is.null (apply.to))
     chk.hy (apply.to)
@@ -105,14 +102,22 @@ spc.fit.poly <- function (fit.to, apply.to = NULL, poly.order = 1, offset.wl = !
 ##' @param npts.min minimal number of points used for fitting the polynomial
 ##' @param noise noise level to be considered during the fit. It may be given
 ##'   as one value for all the spectra, or for each spectrum separately.
+##' @param debuglevel  additional output:
+##'    \code{1} show \code{npts.min}, \code{2} plots support points for 1st spectrum,
+##'    \code{3} plots support points for all spectra.
+##' @seealso  see \code{\link[hyperSpec]{options}} for more on \code{debuglevel}
 ##' @export
 ##' @examples
 ##'
 ##' baselines <- spc.fit.poly.below (spc)
 ##' plot (spc - baselines)
 ##'
+##' spc.fit.poly.below(chondro [1:3], debuglevel = 1)
+##' spc.fit.poly.below(chondro [1:3], debuglevel = 2)
+##' spc.fit.poly.below(chondro [1:3], debuglevel = 3, noise = sqrt (rowMeans (chondro [[1:3]])))
 spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
-                                npts.min = NULL, noise = 0, offset.wl = FALSE,
+                                npts.min = max (round (nwl (fit.to) * 0.05), 3 * (poly.order + 1)),
+                                noise = 0, offset.wl = FALSE,
                                 debuglevel = hy.getOption("debuglevel")){
   chk.hy (fit.to)
   if (! is.null (apply.to))
@@ -121,10 +126,10 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
   validObject (fit.to)
   validObject (apply.to)
 
-  if (is.null (npts.min)){
-    npts.min <- max (round (nwl(fit.to) * 0.05), 3 * (poly.order + 1))
-    cat ("Fitting with npts.min = ",  npts.min, "\n")
-  } else  if (npts.min <= poly.order){
+  if (missing (npts.min) && debuglevel >= 1L)
+    message ("Fitting with npts.min = ",  npts.min, "\n")
+
+  if (npts.min <= poly.order){
     npts.min <- poly.order + 1
     warning (paste ("npts.min too small: adjusted to", npts.min))
   }
@@ -148,21 +153,20 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
   for (i in row.seq (fit.to)){
     use.old <- logical (nwl (fit.to))
     use <- !use.old
-    
-    if (debuglevel > 0) {
+
+    if (debuglevel == 2L && i == 1L || debuglevel >= 3L) {
       plot(fit.to [i], title.args = list (main = paste ("spectrum", i)))
-      cl <- matlab.dark.palette(25)
       iter <- 0
     }
-    
+
     repeat {
       p[i,] <- qr.solve (vdm[use,], y[use, i])
       bl <- vdm %*% p [i,]
       use.old <- use
       use <- y[, i] < bl + noise [i]
 
-      if (debuglevel > 0) {
-        plot (fit.to[,, use.old], add = TRUE, lines.args = list (pch = 20, type = "p"), col= (iter %% 8) + 1);
+      if (debuglevel == 2L && i == 1L || debuglevel >= 3L) {
+        plot (fit.to[i,, use], add = TRUE, lines.args = list (pch = 20, type = "p"), col= (iter %% 8) + 2);
         lines (fit.to@wavelength, bl, col=(iter %% 8) + 1);
         iter <- iter + 1
       }
@@ -170,6 +174,9 @@ spc.fit.poly.below <- function (fit.to, apply.to = fit.to, poly.order = 1,
       if ((sum (use, na.rm=TRUE) < npts.min) || all (use == use.old, na.rm = TRUE))
         break
     }
+
+    if (debuglevel >= 1L)
+      message (sprintf ("spectrum % 6i: % 5i support points, noise = %0.1f", i, sum (use.old, na.rm = TRUE), noise [i]))
   }
   if (is.null (apply.to)){
     fit.to <- new("hyperSpec", spc=p, wavelength=0 : poly.order)
