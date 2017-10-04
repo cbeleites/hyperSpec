a1d36ceea92e39d487da832f61350890b6c2512e
Claudia Beleites <Claudia.Beleites@chemometrix.eu>
2017-10-04 13:57:07 +0200

    update rbind.fill.matrix

--- hyperSpec/R/rbind.fill.R
+++ hyperSpec/R/rbind.fill.R
@@ -1,65 +1,5 @@
-### These functions were thought to move eventually into Hadley Wickham's plyr. This is not going to
-### happen, soon, however.
-
-##' Bind matrices by row, and fill missing columns with NA
-##'
-##' The matrices are bound together using their column names or the column indices (in that order of
-##' precedence.) Numeric columns may be converted to character beforehand, e.g. using format.
-##' If a matrix doesn't have colnames, the column number is used.
-##'
-##' @author C. Beleites
-##' @seealso   \code{\link[base]{rbind}}, \code{\link[base]{cbind}}, \code{\link[plyr]{rbind.fill}}
-##' @export
-##' @keywords manip
-##' @rdname rbind-fill
-##' @examples 
-##'  A <- matrix (1:4, 2)
-##'  B <- matrix (6:11, 2)
-##'  A
-##'  B
-##'  rbind.fill.matrix (A, B)
-##' 
-##'  colnames (A) <- c (3, 1)
-##'  A
-##'  rbind.fill.matrix (A, B)
-##' 
-##' @return a matrix
-rbind.fill.matrix <- function (...){
-  matrices <- list (...)
-  
-  ## check the arguments
-  if (! all  (sapply (matrices, is.matrix)))
-    stop ("Input ", which (! sapply (matrices, is.matrix)), "is no matrix.")
-  
-  ## if the matrices have column names, use them 
-  lcols <- lapply (matrices, .cols)
-  cols  <- unique (unlist (lcols))
-
-  ## preallocate the new spectra matrix
-  pos <- sapply (matrices, nrow)
-  result <- matrix (NA, nrow = sum (pos), ncol = length (cols))
-
-  ## make an index vector for the row positions
-  pos <- c (0, cumsum (pos))
-
-  ## fill in the new matrix 
-  for (i in seq_along (matrices)){
-    icols <- match (lcols[[i]], cols)
-    result [(pos [i] + 1) : pos [i + 1], icols] <- matrices [[i]]
-  }
-
-  attr (result, "cols") <- cols
-  colnames (result) <- cols
-  
-  result
-}
-
-.cols <- function (x){
-  cln <- colnames (x)
-  if (is.null (cln)) cln <- seq_len (ncol (x))
-
-  cln
-}
+### plyr has rbind.fill for matrices now. We keep with our version to avoid the 
+### dependency, but do not export it anymore.
 
 
 ##' Quick data frame.
@@ -100,26 +40,25 @@ quickdf <- function(list) {
 ##'
 ##' The return matrix will always have column names.
 ##'
-##' @aliases rbind.fill
-##' @rdname rbind-fill
 ##' @author C. Beleites
 ##' @seealso   \code{\link[base]{rbind}}, \code{\link[base]{cbind}}, \code{\link[plyr]{rbind.fill}}
-##' @rdname rbind-fill
 ##' @keywords manip
+##' @rdname rbind.fill
 ##' @examples 
 ##'  A <- matrix (1:4, 2)
 ##'  B <- matrix (6:11, 2)
 ##'  A
 ##'  B
-##'  rbind.fill.matrix (A, B)
+##'  hyperSpec:::rbind.fill.matrix (A, B)
 ##' 
 ##'  colnames (A) <- c (3, 1)
 ##'  A
-##'  rbind.fill.matrix (A, B)
+##'  hyperSpec:::rbind.fill.matrix (A, B)
 ##'
-##'  rbind.fill.matrix (A, 99)
+##'  hyperSpec:::rbind.fill.matrix (A, 99)
 ##' 
 ##' @return a matrix
+##' @method rbind.fill matrix
 rbind.fill.matrix <- function (...){
   matrices <- list (...)
   
@@ -174,7 +113,7 @@ rbind.fill.matrix <- function (...){
 ##'  
 ##' @param ... data frames/matrices to row bind together
 ##' @keywords manip
-##' @rdname rbind-fill
+##' @rdname rbind.fill
 ##' @examples
 ##' #' rbind.fill(mtcars[c("mpg", "wt")], mtcars[c("wt", "cyl")])
 rbind.fill <- function(...) {
