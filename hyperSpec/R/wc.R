b596026b0e1f34ca55b448d8eb406e5aca060b3c
Claudia Beleites <claudia.beleites@chemometrix.gmbh>
2020-01-31 13:45:00 +0100

    Fix issue #97: new function to count lines, wc deprecated

--- hyperSpec/R/wc.R
+++ hyperSpec/R/wc.R
@@ -1,24 +1,30 @@
-##' wc
-##' word count of ASCII files
+##' line/word/character count of ASCII files
 ##'
-##' wc uses the system command wc
+##' `wc()` uses the system command `wc`. Use at your own risk.
+##' @note `wc()` now is deprecated and will be removed from hyperSpec in future. Consider using [count_lines()] instead for line counting.
 ##'
 ##' @param file the file name or pattern
 ##' @param flags the parameters to count, character vector with the long form
 ##'   of the parameters
-##' @return data.frame with the counts and file names, or \code{NULL} if wc is
-##'   not available
+##' @return data.frame with the counts and file names, or `NULL` if wc is
+##'   not available on the system.
+##' @seealso [count_lines()]
 ##' @export
 ##' @author C. Beleites
 ##' @importFrom utils read.table
 wc <- function (file, flags = c("lines", "words", "bytes")){
-  if (length (system ("wc --help", intern = TRUE)) == 0)
+  .Deprecated(new = "count_lines", 
+              msg = "wc() is soft-deprecated: while it will not be removed in the near future, use it at your own risk. The functionality is not routinely tested.")
+  
+  output <- try (system2 ("wc", args = "--help", stdout = TRUE, stderr = TRUE), silent = TRUE) 
+  if (class (output) == "try-error")
     return (NULL)
 
-  wc <- paste ("wc", paste ("--", flags, sep = "", collapse = " "), file)
-  wc <- read.table(pipe (wc))
-  colnames (wc) <- c(flags, "file")
-  wc
+  output <- paste ("wc", paste ("--", flags, sep = "", collapse = " "), file)
+  output <- read.table(pipe (output))
+  colnames (output) <- c(flags, "file")
+  
+  output
 }
 
 .test (wc) <- function (){
@@ -26,32 +32,43 @@ wc <- function (file, flags = c("lines", "words", "bytes")){
 
   tmpfile <- tempfile()
   on.exit (unlink (tmpfile))
-
   writeLines("blabla\nblubb", con = tmpfile)
 
-  res <- wc (tmpfile)
 
-  ## wc does not exist on all systems
-  if (is.null (res)) skip ("wc not available")
-
+  test_that ("depreciation warning", 
+             expect_warning(wc (tmpfile), "soft-deprecated")
+  )
+    
   test_that("wc defaults", {
+    skip_if_not_fileio_available() # see issue #97
+
+    suppressWarnings(res <- wc (tmpfile))
+    
+    if (is.null (res))
+      skip ("wc not available")
+
     if (.Platform$OS.type == "windows")
       expect_equal(res,  data.frame (lines = 2,
-                                     words = 2, 
+                                     words = 2,
                                      bytes = 15, ## additional CR bytes on windows.
                                      file = tmpfile))
     else
-      expect_equal(res,  data.frame (lines = 2, 
-                                     words = 2, 
+      expect_equal(res,  data.frame (lines = 2,
+                                     words = 2,
                                      bytes = 13,
                                      file = tmpfile))
-  }
-  )
-  
-  
+  })
 
-  test_that("wc --lines",
-            expect_equal(wc (file = tmpfile, flags = "lines"),  data.frame (lines = 2, file = tmpfile))
-  )
+
+
+  test_that("wc --lines", {
+    skip_if_not_fileio_available() # see issue #97
+
+    suppressWarnings(res <- wc (tmpfile, flags = "lines"))
+    if (is.null (res))
+      skip ("wc not available")
+
+    expect_equal(res,  data.frame (lines = 2, file = tmpfile))
+  })
 
 }
