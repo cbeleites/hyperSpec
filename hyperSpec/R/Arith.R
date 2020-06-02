34e24caa3fb487c1a01169c54425e13ca89e5ee4
Claudia Beleites <claudia.beleites@chemometrix.gmbh>
2020-06-02 15:55:14 +0200

    urgent push in the middle of refactoring and unit test writing

--- hyperSpec/R/Arith.R
+++ hyperSpec/R/Arith.R
@@ -1,3 +1,51 @@
+## arithmetic function called with both parameters hyperSpec
+.arith <- function(e1, e2) {
+  validObject(e1)
+  validObject(e2)
+
+  e1 <- .expand(e1, dim(e2) [c(1, 3)])
+  e2 <- .expand(e2, dim(e1) [c(1, 3)])
+
+  if (ncol(e2) > 1) {
+    warning("Dropping column(s) of e2: ",
+            paste(colnames(e2$..), collapse = ", "))
+  }
+
+  e1[[]] <- callGeneric(e1[[]], e2[[]])
+  e1
+}
+
+.test(.arith) <- function(){
+  context("Arithmetic operators, 2 hyperSpec objects")
+
+  tmp <- as.hyperSpec(matrix(1:length(flu[[]]), nrow = nrow(flu)))
+  tmp[[]] <- 1:length(tmp[[]])
+  tmp$testcolumn <- 1:nrow(tmp)
+
+  test_that("correct results for 2 equal-sized objects", {
+    for (operator in c(`+`, `-`, `*`, `/`, `^`, `%%`, `%/%`)) {
+
+      expect_warning(res <- operator(flu, tmp),
+                     "Dropping column[(]s[)] of e2: testcolumn")
+
+      expect_equal(res[[]], operator(flu[[]], tmp[[]]))
+      expect_equal(res$.., flu$..)
+      expect_equal(dim(res), dim(flu))
+      expect_equal(wl(res), wl(flu))
+    }
+  })
+
+  test_that("correct results with row-sized object", {
+  })
+
+  test_that("correct results with column-sized object", {
+  })
+
+  test_that("correct results with 1x1 object", {
+  })
+
+}
+
 ##' Arithmetical Operators: +, -, *, /, ^, %%, %/%, %*% for hyperSpec objects
 ##'
 ##' The arithmetical operators `+`, `-`, `*`, `/`, `^`, `%%`, `%/%`, and `%*%`
@@ -24,8 +72,6 @@
 ##' If you want to calculate on the extra data as well, use the data.frame
 ##' `hyperSpec@data` directly or [`as.data.frame(x)`][as.data.frame()].
 ##' @author C. Beleites
-##' @title Arithmetical Operators for hyperSpec objects
-##' @name Arith
 ##' @md
 ##' @rdname Arith
 ##' @docType methods
@@ -64,39 +110,124 @@
 ##' all((flu + flu - 2 * flu)[[]] == 0)
 ##' -flu
 ##' flu / flu$c
+setMethod("Arith", signature(e1 = "hyperSpec", e2 = "hyperSpec"), .arith)
 
-
-setMethod(
-  "Arith", signature(e1 = "hyperSpec", e2 = "hyperSpec"),
-  function(e1, e2) {
-    validObject(e1)
-    validObject(e2)
-
-    e1 <- .expand(e1, dim(e2) [c(1, 3)])
-    e2 <- .expand(e2, dim(e1) [c(1, 3)])
-
-    e1 [[]] <- callGeneric(e1[[]], e2[[]])
-    e1
-  }
-)
-
+## arithmetic function called with first parameter hyperSpec
 .arithx <- function(e1, e2) {
   validObject(e1)
 
   if (missing(e2)) {
-    e1  [[]] <- callGeneric(e1 [[]])
+    e1[[]] <- callGeneric(e1[[]])
     e1
   } else {
-    e2 <- as.matrix(e2)
-
-    ## called /only/ with e1 hyperSpec but e2 matrix-like
+    ## called /only/ with e1 hyperSpec but e2 numeric
     e1 <- .expand(e1, dim(e2))
     e2 <- .expand(e2, dim(e1) [c(1, 3)])
 
-    e1  [[]] <- callGeneric(e1 [[]], e2)
+    e1[[]] <- callGeneric(e1[[]], e2)
     e1
   }
 }
+
+.test(.arithx) <- function(){
+  context("Arithmetic operators, hyperSpec object x")
+
+  test_that("correct results with scalar", {
+    for (operator in c(`+`, `-`, `*`, `/`, `^`, `%%`, `%/%`)) {
+      res <- operator(flu, 2)
+      expect_equal(res[[]], operator(flu[[]], 2))
+      expect_equal(res$.., flu$..)
+      expect_equal(dim(res), dim(flu))
+      expect_equal(wl(res), wl(flu))
+    }
+  })
+
+  test_that("correct results with vector for columns", {
+    v <- 1:nrow(flu)
+
+    for (operator in c(`+`, `-`, `*`, `/`, `^`, `%%`, `%/%`)) {
+      res <- operator(flu, v)
+
+      expect_equal(res, sweep(flu, 1, v, FUN = operator))
+    }
+  })
+
+    test_that("correct results with vector for rows", {
+    v <- 1:nwl(flu)
+
+    for (operator in c(`+`, `-`, `*`, `/`, `^`, `%%`, `%/%`)) {
+      res <- operator(flu, v)
+
+      expect_equal(res, sweep(flu, 2, v, FUN = operator))
+    }
+  })
+
+
+  test_that("correct results with matrix", {
+    m <- matrix(1:length(flu[[]]), nrow = nrow(flu))
+
+    for (operator in c(`+`, `-`, `*`, `/`, `^`, `%%`, `%/%`)) {
+      res <- operator(flu, m)
+      expect_equal(res[[]], operator(flu[[]], m))
+      expect_equal(res$.., flu$..)
+      expect_equal(dim(res), dim(flu))
+      expect_equal(wl(res), wl(flu))
+    }
+  })
+
+  test_that("correct results with missing 2nd parameter/unary operators", {
+    for (operator in c(`+`, `-`)) {
+      res <- operator(flu)
+      expect_equal(res[[]], operator(flu[[]]))
+      expect_equal(res$.., flu$..)
+      expect_equal(dim(res), dim(flu))
+      expect_equal(wl(res), wl(flu))
+    }
+    for (operator in c(`*`, `/`, `^`, `%%`, `%/%`))
+      expect_error(operator(flu))
+  })
+
+
+  test_that("binary -", {
+    expect_warning(res <- flu - flu)
+    expect_equal(
+      as.matrix(res),
+      matrix(0, nrow = nrow(flu), ncol = nwl(flu), dimnames = dimnames(flu[[]]))
+    )
+
+    expect_warning(res <- flu - flu[1])
+    expect_equal(as.matrix(res), as.matrix(sweep(flu, 2, flu[1], `-`)))
+
+    expect_warning(res <- flu - flu[, , 450])
+    expect_equal(as.matrix(res), as.matrix(sweep(flu, 1, flu[, , 450], `-`)))
+  })
+
+  test_that("binary /", {
+    expect_warning(res <- flu / flu)
+    expect_equal(
+      as.matrix(res),
+      matrix(1, nrow = nrow(flu), ncol = nwl(flu), dimnames = dimnames(flu[[]]))
+    )
+
+    expect_warning(res <- flu / flu[1])
+    expect_equal(as.matrix(res), as.matrix(sweep(flu, 2, flu[1], `/`)))
+
+    expect_warning(res <- flu / flu[, , 450])
+    expect_equal(as.matrix(res), as.matrix(sweep(flu, 1, flu [, , 450], `/`)))
+  })
+
+  test_that("binary + with scalar", {
+    expect_equal(as.matrix(flu + 1), as.matrix(flu) + 1)
+    expect_equal(as.matrix(1 + flu), as.matrix(flu) + 1)
+  })
+
+  test_that("unary -", {
+    expect_equal(as.matrix(-flu), -as.matrix(flu))
+  })
+
+}
+
+
 ##' @rdname Arith
 setMethod("Arith", signature(e1 = "hyperSpec", e2 = "numeric"), .arithx)
 ##' @rdname Arith
@@ -104,15 +235,16 @@ setMethod("Arith", signature(e1 = "hyperSpec", e2 = "matrix"), .arithx)
 ##' @rdname Arith
 setMethod("Arith", signature(e1 = "hyperSpec", e2 = "missing"), .arithx)
 
+## arithmetic function called with second parameter hyperSpec
 .arithy <- function(e1, e2) {
-  e1 <- as.matrix(e1)
+  #e1 <- as.matrix(e1)
   validObject(e2)
 
-  ## called /only/ with e2 hyperSpec but e1 matrix-like
+  ## called /only/ with e2 hyperSpec but e1 numeric
   e1 <- .expand(e1, dim(e2) [c(1, 3)])
   e2 <- .expand(e2, dim(e1))
 
-  e2  [[]] <- callGeneric(e1, e2 [[]])
+  e2[[]] <- callGeneric(e1,e2 [[]])
   e2
 }
 ##' @rdname Arith
@@ -120,27 +252,80 @@ setMethod("Arith", signature(e1 = "numeric", e2 = "hyperSpec"), .arithy)
 ##' @rdname Arith
 setMethod("Arith", signature(e1 = "matrix", e2 = "hyperSpec"), .arithy)
 
-##' @param m matrix
+##' expand a row- or column vector (or matrix of size 1 x n or n x 1)
+##' along the size-1-dimension.
+##'
+##' Dimensions that have size > 1 are ignored (they lead to an error in the
+##' arithmetic function since the size does not match)
+##'
+##' @param m matrix, vector or scalar
 ##' @param target.dim target size to expand the vector to for the sweep-shortcuts
 ##' @noRd
 .expand <- function(m, target.dim) {
+
+  ## vector corresponding to a single row
+  if (is.vector(m) & length(m) > 1 & length(m) == target.dim[2]) {
+    if (length(m) == target.dim[1]) {
+      message("Square target: recycling by column.")
+    } else {
+      m <- t(m)
+    }
+  }
+  m <- as.matrix(m)
   m.dim <- dim(m)
 
-  if (m.dim [1] == 1L & target.dim [1] > 1L) {
-    m <- m [rep(1, target.dim [1]), , drop = FALSE]
+  if (m.dim[1] == 1L & target.dim[1] > 1L) {
+    m <- m[rep(1, target.dim[1]), , drop = FALSE]
   }
 
   if (is(m, "hyperSpec")) {
-    if (m.dim [3] == 1L & target.dim [2] > 1L) {
-      m <- m [, , rep(1, target.dim[2]), wl.index = TRUE]
+    if (m.dim[3] == 1L & target.dim[2] > 1L) {
+      m <- m[, , rep(1, target.dim[2]), wl.index = TRUE]
     }
   } else {
-    if (m.dim [2] == 1L & target.dim[2] > 1L) {
-      m <- m [, rep(1, target.dim [2]), drop = FALSE]
+    if (m.dim[2] == 1L & target.dim[2] > 1L) {
+      m <- m[, rep(1, target.dim[2]), drop = FALSE]
     }
   }
 
   m
+}
+
+.test(.expand) <- function(){
+  context(".expand helper function for sweep shortcut operators")
+
+    test_that("scalar", {
+      expect_equal(.expand(1, c(1, 3)), matrix(rep(1, 3), nrow = 1, ncol = 3))
+      expect_equal(.expand(1, c(3, 1)), matrix(rep(1, 3), nrow = 3, ncol = 1))
+      expect_equal(.expand(1, c(3, 5)), matrix(rep(1, 3*5), nrow = 3, ncol = 5))
+      expect_equal(.expand(1, c(1, 1)), matrix(1))
+    })
+
+    test_that("matrix with > 1 row / > 1 column: leave unchanged", {
+      m <- matrix(1:15, ncol = 5)
+
+      expect_equal(.expand(m, c(1, 5)), m)
+      expect_equal(.expand(m, c(3, 1)), m)
+    })
+
+    test_that("matrix with 1 row", {
+      m <- matrix(1:5, ncol = 5)
+
+      expect_equal(.expand(m, c(1, 5)), m)
+      expect_equal(.expand(m, c(3, 5)), m[rep(1, 3),])
+      expect_equal(.expand(m, c(3, 4)), m[rep(1, 3),])  # ignore request of ncol = 4 !!!
+    })
+
+    test_that("matrix with 1 column", {
+      m <- matrix(1:5, nrow = 5)
+
+      expect_equal(.expand(m, c(5, 3)), m[,rep(1, 3)])
+      expect_equal(.expand(m, c(5, 1)), m)
+      expect_equal(.expand(m, c(4, 3)), m[rep(1, 3),]) # ignore request of nrow = 4 !!!
+    })
+
+
+
 }
 
 
@@ -255,37 +440,3 @@ setMethod("%*%", signature(x = "hyperSpec", y = "matrix"), .matmul_hm)
 ##' @rdname Arith
 setMethod("%*%", signature(x = "matrix", y = "hyperSpec"), .matmul_mh)
 
-.test(.arithx) <- function() {
-  context("Arith")
-
-  test_that("binary -", {
-    expect_equal(
-      as.matrix(flu - flu),
-      matrix(0, nrow = nrow(flu), ncol = nwl(flu), dimnames = dimnames(flu [[]]))
-    )
-
-    expect_equal(as.matrix(flu - flu [1]), as.matrix(sweep(flu, 2, flu [1], `-`)))
-
-    expect_equal(as.matrix(flu - flu [, , 450]), as.matrix(sweep(flu, 1, flu [, , 450], `-`)))
-  })
-
-  test_that("binary /", {
-    expect_equal(
-      as.matrix(flu / flu),
-      matrix(1, nrow = nrow(flu), ncol = nwl(flu), dimnames = dimnames(flu [[]]))
-    )
-
-    expect_equal(as.matrix(flu / flu [1]), as.matrix(sweep(flu, 2, flu [1], `/`)))
-
-    expect_equal(as.matrix(flu / flu [, , 450]), as.matrix(sweep(flu, 1, flu [, , 450], `/`)))
-  })
-
-  test_that("binary + with scalar", {
-    expect_equal(as.matrix(flu + 1), as.matrix(flu) + 1)
-    expect_equal(as.matrix(1 + flu), as.matrix(flu) + 1)
-  })
-
-  test_that("unary -", {
-    expect_equal(as.matrix(-flu), -as.matrix(flu))
-  })
-}
