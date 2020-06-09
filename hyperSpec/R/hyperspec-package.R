8a89cd31bb816e6282f962b22753bff9e708f6ee
Erick Oduniyi <eeoduniyi@gmail.com>
2020-06-09 00:08:28 -0500

    Replace `chondro` with `fauxCell` in hyperspec-package.R

--- hyperSpec/R/hyperspec-package.R
+++ hyperSpec/R/hyperspec-package.R
@@ -25,7 +25,7 @@
 ##'   package.
 ##' @rdname hyperSpec-package
 ##' @include flu.R
-##' @include chondro.R
+##' @include fauxCell.R
 ##' @include laser.R
 ##' @include paracetamol.R
 ##' @include barbiturates.R
