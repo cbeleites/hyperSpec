294c23d15d7d1492ec830ac5ff1edb1560f043f1
Claudia Beleites <claudia.beleites@chemometrix.gmbh>
2020-06-09 16:45:34 +0200

    styling

--- hyperSpec/R/spc.fit.poly.R
+++ hyperSpec/R/spc.fit.poly.R
@@ -30,11 +30,12 @@
 ##'
 ##' \dontrun{vignette ("baseline", package = "hyperSpec")}
 ##'
-##' spc <- fauxCell [1 : 10]
-##' baselines <- spc.fit.poly(spc [,, c (625 ~ 640, 1785 ~ 1800)], spc)
+##' spc <- fauxCell[1 : 10]
+##' baselines <- spc.fit.poly(spc[,, c (625 ~ 640, 1785 ~ 1800)], spc)
 ##' plot(spc - baselines)
 ##'
-spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(is.null(apply.to))) {
+spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1,
+                         offset.wl = !(is.null(apply.to))) {
   chk.hy(fit.to)
   if (!is.null(apply.to)) {
     chk.hy(apply.to)
@@ -57,8 +58,8 @@ spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(
   p <- apply(
     fit.to, 1,
     function(y, x) {
-      x <- x [!is.na(y), , drop = FALSE]
-      y <- y [!is.na(y)]
+      x <- x[!is.na(y), , drop = FALSE]
+      y <- y[!is.na(y)]
       qr.solve(x, y)
     },
     x
@@ -87,9 +88,9 @@ spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(
   context("spc.fit.poly")
 
   test_that(
-    "no normalization",
+    "no normalization", {
     bl.nonorm <- spc.fit.poly(flu, flu, poly.order = 3, offset.wl = FALSE)
-  )
+  })
 
   # test effect of wavelength axis normalization
   # was issue 1 on github
@@ -101,17 +102,17 @@ spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(
 
     bl.1e4 <- spc.fit.poly(tmp, tmp, poly.order = 3, offset.wl = TRUE)
     bl.nonorm <- spc.fit.poly(flu, flu, poly.order = 3, offset.wl = FALSE)
-    expect_equal(bl.nonorm [[]], bl.1e4 [[]])
+    expect_equal(bl.nonorm[[]], bl.1e4[[]])
   })
 
   test_that("spectrum containing NA", {
-    tmp <- fauxCell [1]
-    tmp [[, , 1600]] <- NA
+    tmp <- fauxCell[1]
+    tmp[[, , 1600]] <- NA
 
-    coefs <- spc.fit.poly(tmp, apply.to = NULL) [[]]
+    coefs <- spc.fit.poly(tmp, apply.to = NULL)[[]]
     expect_equal(
       coefs,
-      spc.fit.poly(fauxCell [1, , !is.na(tmp)], apply.to = NULL) [[]]
+      spc.fit.poly(fauxCell[1, , !is.na(tmp)], apply.to = NULL)[[]]
     )
 
     ## bug was: all coefficients were silently 0
@@ -121,20 +122,20 @@ spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(
 
 ##'
 ##' \code{spc.fit.poly.below} tries to fit the baseline on appropriate spectral
-##' ranges of the spectra in \code{fit.to}.  For details, see the
-##' \code{vignette ("baseline")}.
+##' ranges of the spectra in \code{fit.to}.  For details, see the \code{vignette
+##' ("baseline")}.
 ##' @rdname baselines
 ##' @param npts.min minimal number of points used for fitting the polynomial
-##' @param noise noise level to be considered during the fit. It may be given
-##'   as one value for all the spectra, or for each spectrum separately.
+##' @param noise noise level to be considered during the fit. It may be given as
+##'   one value for all the spectra, or for each spectrum separately.
 ##' @param max.iter stop at the latest after so many iterations.
-##' @param stop.on.increase additional stopping rule: stop if the number of support points would increase,
-##' regardless whether npts.min was reached or not.
-##' @param debuglevel  additional output:
-##'    \code{1} shows \code{npts.min},
-##'    \code{2} plots support points for the final baseline of 1st spectrum,
-##'    \code{3} plots support points for 1st spectrum,
-##'    \code{4} plots support points for all spectra.
+##' @param stop.on.increase additional stopping rule: stop if the number of
+##'   support points would increase, regardless whether npts.min was reached or
+##'   not.
+##' @param debuglevel  additional output: \code{1} shows \code{npts.min},
+##'   \code{2} plots support points for the final baseline of 1st spectrum,
+##'   \code{3} plots support points for 1st spectrum, \code{4} plots support
+##'   points for all spectra.
 ##' @seealso  see \code{\link[hyperSpec]{options}} for more on \code{debuglevel}
 ##' @export
 ##' @examples
@@ -142,13 +143,16 @@ spc.fit.poly <- function(fit.to, apply.to = NULL, poly.order = 1, offset.wl = !(
 ##' baselines <- spc.fit.poly.below (spc)
 ##' plot (spc - baselines)
 ##'
-##' spc.fit.poly.below(fauxCell [1:3], debuglevel = 1)
-##' spc.fit.poly.below(fauxCell [1:3], debuglevel = 2)
-##' spc.fit.poly.below(fauxCell [1:3], debuglevel = 3, noise = sqrt (rowMeans (fauxCell [[1:3]])))
+##' spc.fit.poly.below(fauxCell[1:3], debuglevel = 1)
+##' spc.fit.poly.below(fauxCell[1:3], debuglevel = 2)
+##' spc.fit.poly.below(fauxCell[1:3], debuglevel = 3,
+##'                    noise = sqrt (rowMeans (fauxCell[[1:3]])))
 ##'
 spc.fit.poly.below <- function(fit.to, apply.to = fit.to, poly.order = 1,
-                               npts.min = max(round(nwl(fit.to) * 0.05), 3 * (poly.order + 1)),
-                               noise = 0, offset.wl = FALSE, max.iter = nwl(fit.to),
+                               npts.min = max(round(nwl(fit.to) * 0.05),
+                                              3 * (poly.order + 1)),
+                               noise = 0, offset.wl = FALSE,
+                               max.iter = nwl(fit.to),
                                stop.on.increase = FALSE,
                                debuglevel = hy.getOption("debuglevel")) {
   ## for debuglevel >= 2L
@@ -185,63 +189,88 @@ spc.fit.poly.below <- function(fit.to, apply.to = fit.to, poly.order = 1,
   }
 
   vdm <- vanderMonde(x, poly.order)
-  y <- t(fit.to [[]])
+  y <- t(fit.to[[]])
 
   p <- matrix(nrow = nrow(fit.to), ncol = poly.order + 1)
   for (i in row.seq(fit.to)) {
     use.old <- logical(nwl(fit.to))
-    use <- !is.na(y [, i])
+    use <- !is.na(y[, i])
 
     if (debuglevel %in% c(2L, 3L) && i == 1L || debuglevel >= 4L) {
-      plot(fit.to [i], title.args = list(main = paste("spectrum", i)))
+      plot(fit.to[i], title.args = list(main = paste("spectrum", i)))
       message("start: ", sum(use, na.rm = TRUE), " support points")
     }
 
     for (iter in 1:max.iter) {
       p[i, ] <- qr.solve(vdm[use, ], y[use, i])
-      bl <- vdm %*% p [i, ]
+      bl <- vdm %*% p[i, ]
       use.old <- use
-      use <- y[, i] < bl + noise [i] & !is.na(y [, i])
+      use <- y[, i] < bl + noise[i] & !is.na(y[, i])
 
       if (debuglevel == 3L && i == 1L || debuglevel >= 4L) {
-        plot(fit.to[i, , use], add = TRUE, lines.args = list(pch = 20, type = "p"), col = cols [iter])
-        lines(fit.to@wavelength, bl, col = cols [iter])
-        lines(fit.to@wavelength, bl + noise, col = cols [iter], lty = 2)
-        message("Iteration ", iter, ": ", sum(use, na.rm = TRUE), " support points")
+
+        plot(fit.to[i, , use], add = TRUE,
+             lines.args = list(pch = 20, type = "p"), col = cols[iter])
+
+        lines(fit.to@wavelength, bl, col = cols[iter])
+        lines(fit.to@wavelength, bl + noise, col = cols[iter], lty = 2)
+
+        message("Iteration ", iter, ": ", sum(use, na.rm = TRUE),
+                " support points")
       }
 
-      if ((sum(use, na.rm = TRUE) < npts.min) || all(use == use.old, na.rm = TRUE)) {
+      if ((sum(use, na.rm = TRUE) < npts.min) ||
+          all(use == use.old, na.rm = TRUE)) {
+
         break
       }
 
-      if (sum(use, na.rm = TRUE) > sum(use.old, na.rm = TRUE) && stop.on.increase) {
+      if (sum(use, na.rm = TRUE) > sum(use.old, na.rm = TRUE) &&
+          stop.on.increase) {
+
         warning(
-          "Iteration ", iter, ": Number of support points is about to increase again. Stopping with ",
-          sum(use.old, na.rm = TRUE), " support points, but this is a local minimum only."
+          "Iteration ", iter,": ",
+          "Number of support points is about to increase again. ",
+          "Stopping with ", sum(use.old, na.rm = TRUE),
+          " support points, but this may be a local minimum."
         )
+
         break
       }
     }
 
     if (iter == max.iter) {
+
       if ((sum(use.old, na.rm = TRUE) == npts.min) &&
         !all(use == use.old, na.rm = TRUE) &&
         !sum(use, na.rm = TRUE) < npts.min) {
-        warning("Reached npts.min, but the solution is not stable. Stopped after ", iter, " iterations.")
+
+        warning("Reached npts.min, but the solution is not stable. ",
+          "Stopped after ", iter, " iterations.")
+
       } else if (sum(use, na.rm = TRUE) >= npts.min) {
-        warning("Stopped after ", iter, " iterations with ", sum(use.old, na.rm = TRUE), " support points.")
+
+        warning("Stopped after ", iter, " iterations with ",
+                sum(use.old, na.rm = TRUE), " support points.")
       }
+
     }
 
     if (debuglevel >= 1L) {
-      message(sprintf("spectrum % 6i: % 5i support points, noise = %0.1f, %3i iterations", i, sum(use.old, na.rm = TRUE), noise [i], iter))
+      message(sprintf("spectrum % 6i: % 5i support points, noise = %0.1f, %3i iterations",
+                      i, sum(use.old, na.rm = TRUE), noise[i], iter))
     }
+
     if ((debuglevel == 2L) && (i == 1L)) {
-      plot(fit.to[i, , use.old], add = TRUE, lines.args = list(pch = 20, type = "p"), col = cols [iter])
-      lines(fit.to@wavelength, bl, col = cols [iter])
-      lines(fit.to@wavelength, bl + noise, col = cols [iter], lty = 2)
+      plot(fit.to[i, , use.old], add = TRUE,
+           lines.args = list(pch = 20, type = "p"), col = cols[iter])
+
+      lines(fit.to@wavelength, bl, col = cols[iter])
+
+      lines(fit.to@wavelength, bl + noise, col = cols[iter], lty = 2)
     }
   }
+
   if (is.null(apply.to)) {
     fit.to <- new("hyperSpec", spc = p, wavelength = 0:poly.order)
     colnames(fit.to@data$spc) <- paste0("(x - minx)^", 0:poly.order)
@@ -294,7 +323,7 @@ spc.fit.poly.below <- function(fit.to, apply.to = fit.to, poly.order = 1,
 
   test_that("stoppng rules for unstable solutions - issue #58", {
     # test object origninally created from chondro:
-    # tmp <- chondro [103,,c(600 ~ 700, 1650 ~ 1800)]
+    # tmp <- chondro[103,,c(600 ~ 700, 1650 ~ 1800)]
     # tmp[[]] <- round(tmp[[]], digits = 1)
 
     tmp <- t(c(331.8, 336.7, 325.3, 313.2, 328.6, 348.5, 304.6, 286.8, 283.9,
@@ -315,13 +344,13 @@ spc.fit.poly.below <- function(fit.to, apply.to = fit.to, poly.order = 1,
   })
 
   test_that("spectrum containing NA", {
-    tmp <- fauxCell [1]
-    tmp [[, , 1600]] <- NA
+    tmp <- fauxCell[1]
+    tmp[[, , 1600]] <- NA
 
-    coefs <- spc.fit.poly.below(tmp, apply.to = NULL) [[]]
+    coefs <- spc.fit.poly.below(tmp, apply.to = NULL)[[]]
     expect_equal(
       coefs,
-      spc.fit.poly.below(fauxCell [1, , !is.na(tmp)], apply.to = NULL) [[]]
+      spc.fit.poly.below(fauxCell[1, , !is.na(tmp)], apply.to = NULL)[[]]
     )
 
     ## bug was: all coefficients were silently 0
