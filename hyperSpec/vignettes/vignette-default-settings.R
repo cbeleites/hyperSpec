292e2bff10e9e79c8758c36e5a162fcc07c18be1
Vilmantas Gegzna <GegznaV@gmail.com>
2020-06-17 20:31:46 +0300

    Update vignette-default-settings.R

--- hyperSpec/vignettes/vignette-default-settings.R
+++ hyperSpec/vignettes/vignette-default-settings.R
@@ -9,13 +9,63 @@ knitr::knit_hooks$set(small.mar = function(before, options, envir) {
 knitr::opts_chunk$set(small.mar = TRUE)
 
 # Lattice parameters ---------------------------------------------------------
-trellis.pars <- trellis.par.get("layout.heights")
-trellis.pars[grep("padding", names(trellis.pars))] <- 0
-trellis.par.set(layout.heights = trellis.pars)
+set_trellis_layout_hw_default <- function() {
 
-trellis.pars <- trellis.par.get("layout.widths")
-trellis.pars[grep("padding", names(trellis.pars))] <- 0
-trellis.par.set(layout.widths = trellis.pars)
+  trellis_layout_heights <- list(
+    top.padding = 1,
+    main = 1,
+    main.key.padding = 1,
+    key.top = 1,
+    xlab.top = 1,
+    key.axis.padding = 1,
+    axis.top = 1,
+    strip = 1,
+    panel = 1,
+    axis.panel = 1,
+    between = 1,
+    axis.bottom = 1,
+    axis.xlab.padding = 1,
+    xlab = 1,
+    xlab.key.padding = 0,
+    key.bottom = 1,
+    key.sub.padding = 1,
+    sub = 1,
+    bottom.padding = 1
+  )
+
+  trellis_layout_widths <- list(
+    left.padding = 1,
+    key.left = 1,
+    key.ylab.padding = 0,
+    ylab = 1,
+    ylab.axis.padding = 1,
+    axis.left = 1,
+    axis.panel = 1,
+    strip.left = 1,
+    panel = 1,
+    between = 1,
+    axis.right = 1,
+    axis.key.padding = 1,
+    ylab.right = 1,
+    key.right = 1,
+    right.padding = 1
+  )
+
+  trellis.par.set(layout.heights = trellis_layout_heights)
+  trellis.par.set(layout.widths = trellis_layout_widths)
+}
+
+set_trellis_layout_hw_custom <- function() {
+  trellis_layout_heights <- trellis.par.get("layout.heights")
+  trellis_layout_heights[grep("padding", names(trellis_layout_heights))] <- 0
+  trellis.par.set(layout.heights = trellis_layout_heights)
+
+  trellis_layout_widths <- trellis.par.get("layout.widths")
+  trellis_layout_widths[grep("padding", names(trellis_layout_widths))] <- 0
+  trellis.par.set(layout.widths = trellis_layout_widths)
+}
+
+set_trellis_layout_hw_custom()
 
 # knitr Settings ===================================================================
 knitr::opts_chunk$set(
@@ -30,5 +80,5 @@ knitr::opts_chunk$set(
 
 # R Settings ==========================================================
 options("width" = 100, "digits" = 5)
-options(rmarkdown.html_vignette.check_title = FALSE) # Vig index entry ne document title
+options(rmarkdown.html_vignette.check_title = FALSE) # Vig index entry not document title
 set.seed(2020)
