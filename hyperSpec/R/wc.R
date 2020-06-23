0dc624b5fddf11d1c821ae5af8e01eaf3f964261
Vilmantas Gegzna <GegznaV@gmail.com>
2020-06-23 14:19:25 +0300

    Add keyword `@deprecated`

--- hyperSpec/R/wc.R
+++ hyperSpec/R/wc.R
@@ -10,6 +10,7 @@
 #' @return data.frame with the counts and file names, or `NULL` if wc is
 #'   not available on the system.
 #' @seealso [count_lines()]
+#' @concept deprecated
 #' @export
 #' @author C. Beleites
 #' @importFrom utils read.table
