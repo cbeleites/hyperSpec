6cc88db1d1b54766970af0e1637d35a0bb3a80ff
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-19 00:38:43 +0300

    Make `cite_pkg()` more robust
    The main changes:
    
    - order of input variables was changed to make it work if `bib` is not provided.
    - `prefix` is used inside the function
    - the same default value of `prefix`  is used by `cite_pkg` and by `knitr::write_bib`
    - the output to console (side effect) of `knitr::write_bib` is suppessed

--- hyperSpec/vignettes/vignette-functions.R
+++ hyperSpec/vignettes/vignette-functions.R
@@ -4,19 +4,30 @@
 #'
 #' [cite_pkg()] - cites all references provided in a package.
 #'
-#' @param bib bibliography as returned invisibly by [knitr::write_bib()]
-#' @param ... (character) Names of packages to cite
-#' @md
+#' @param ... (character) Names of packages to cite.
+#' @param bib bibliography as returned invisibly by [knitr::write_bib()].
+#' @param prefix (character) Prefix string for keys in BibTeX entries;
+#'        by default, it is `R-` unless `option('knitr.bib.prefix')` has been
+#'        set to another string. The `prefix` should contain no symbols that
+#'        have special meaning in regular expressions.
+#'
 #' @examples
 #' cite_pkg()
 #'
 #' cite_pkg("Rcpp")
+#'
+#' cite_pkg("Rcpp", prefix = "")
+#'
+#' cite_pkg("Rcpp", prefix = "pkg-")
 
-cite_pkg <- function(bib = knitr::write_bib(...), ..., prefix = "R-") {
+cite_pkg <- function(...,
+  bib = {capture.output({x <- knitr::write_bib(..., prefix = prefix)}); x},
+  prefix = getOption("knitr.bib.prefix", "R-")) {
+
   keys <- sapply(bib, function(x) x[[1]])
   keys <- gsub("@[A-Za-z]*[{]|,$", "", keys)
 
-  keys <- grep(paste0("^(R-)?", ...), keys, value = TRUE)
+  keys <- grep(paste0("^(", prefix, ")?", ...), keys, value = TRUE)
 
-  paste0(paste0("@", keys, collapse = ";"))
+  paste0("@", keys, collapse = ";")
 }
