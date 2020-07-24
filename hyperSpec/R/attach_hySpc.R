3d46d4a7f86c21cf53872914c4bcd14989417c3c
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-24 23:24:04 +0300

    Rename variables, improve documentation

--- hyperSpec/R/attach_hySpc.R
+++ hyperSpec/R/attach_hySpc.R
@@ -1,22 +1,24 @@
-#' Load all installed packages of the hySpc.\* package family
+#' Load all installed packages of the hySpc.\* package family.
 #'
 #' This function loads all installed packages of the **hySpc._something_**
 #' package family.
 #'
-#' @param dont_attach Packages that should *not* be attached
-#' @param ... further parameters are handed to [base::library()]
+#' @param dont_attach Packages that should *not* be attached.
+#' @param ... Further parameters are handed to [base::library()].
 #'
 #' @export
 #' @importFrom utils installed.packages
+#' @concept hyperSpec-main
 #' @examples
 #' attach_hySpc()
-attach_hySpc <- function(dont_attach = "hySpc.testthat", ...){
-  hySpc_packages <- installed.packages()
-  hySpc_packages <- grep("^hySpc[.]", row.names(hySpc_packages), value = TRUE)
+
+attach_hySpc <- function(dont_attach = "hySpc.testthat", ...) {
+  installed_pkgs <- row.names(installed.packages())
+  hySpc_packages <- grep("^hySpc[.]", installed_pkgs, value = TRUE)
 
   hySpc_packages <- setdiff(hySpc_packages, dont_attach)
 
-  lapply(hySpc_packages, function(x){
+  lapply(hySpc_packages, function(x) {
     do.call("library", list(package = x, ...))
     }
   )
