ea5e3c5d3f8989a11e134752b1dfdfe235c6cd55
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-25 00:55:00 +0300

    Add mechanism to suppress messages
    Prevents from cluttering the console, if needed.

--- hyperSpec/R/attach_hySpc.R
+++ hyperSpec/R/attach_hySpc.R
@@ -5,6 +5,13 @@
 #'
 #' @param dont_attach Packages that should *not* be attached.
 #' @param ... Further parameters are handed to [base::library()].
+#' @param quiet (`TRUE`|`NA`|`FALSE`) Controls, which messages should be
+#'        printed:
+#'
+#' - `FALSE`: no messages will be printed to the console.
+#' - `NA` (default): short message with the names of `hySpc.*` packages to
+#'    attach will be printed, but package startup messages will be suppressed.
+#' - `TRUE`: both short and package startup messages will be printed.
 #'
 #' @return Invisibly returns a list with the output of [base::library()] for
 #'   each **`hySpc.*`** package that was attached.
@@ -13,29 +20,46 @@
 #' @importFrom utils installed.packages
 #' @concept hyperSpec-main
 #' @examples
+#' \dontrun{
 #' attach_hySpc()
+#'
+#' attach_hySpc(quiet = TRUE)
+#' }
 
-attach_hySpc <- function(dont_attach = "hySpc.testthat", ...) {
+attach_hySpc <- function(dont_attach = "hySpc.testthat", ..., quiet = NA) {
   installed_pkgs <- row.names(installed.packages())
   hySpc_packages <- c("hyperSpec", grep("^hySpc[.]", installed_pkgs, value = TRUE))
 
   hySpc_packages <- setdiff(hySpc_packages, unique(c(dont_attach, .packages())))
 
-  if (length(hySpc_packages) > 0) {
-    message(
-      "\n-----------------------------------------------------------\n",
-      "The following `hySpc` family packages are being attached: \n\n",
-      paste0("  ", hySpc_packages, collapse = "\n"), "\n",
-      "\n-----------------------------------------------------------\n"
-    )
+  if (is.na(quiet) || isFALSE(quiet)) {
 
-  } else {
-    message("\nAll installed `hySpc` family packages are already attached.\n")
+    if (length(hySpc_packages) > 0) {
+      message(
+        "\n-----------------------------------------------------------\n",
+        "The following `hySpc` family packages are being attached: \n\n",
+        paste0("  ", hySpc_packages, collapse = "\n"), "\n",
+        "\n-----------------------------------------------------------\n"
+      )
+    } else {
+      message("\nAll installed `hySpc` family packages are already attached.\n")
+    }
+
   }
 
-  out <- lapply(hySpc_packages, function(x) {
-    do.call("library", list(package = x, ...))
-  })
+  attach_pkgs <- function() {
+    lapply(hySpc_packages, function(x) {
+      do.call("library", list(package = x, ...))
+    })
+  }
+
+  out <-
+    if (is.na(quiet) || isTRUE(quiet)) {
+      suppressPackageStartupMessages({attach_pkgs()})
+
+    } else {
+    attach_pkgs()
+  }
 
   invisible(out)
 }
