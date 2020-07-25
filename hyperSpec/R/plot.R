7ae944e16b82d0fa67a539dde888b91f4daa9d2b
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-26 01:09:26 +0300

    Format documentation

--- hyperSpec/R/plot.R
+++ hyperSpec/R/plot.R
@@ -86,41 +86,41 @@ setGeneric("plot")
 #'
 #' It also supplies some convenient abbrevations for much used plots.
 #'
-#' If `y` is missing, `plot` behaves like `plot (x, y = "spc")`.
+#' If `y` is missing, `plot` behaves like `plot(x, y = "spc")`.
 #'
 #' Supported values for `y` are:
 #'
-#' \describe{ \item{"spc"}{calls [plotspc()] to produce a spectra
-#' plot.}
+#' \describe{
+#'
+#' \item{"spc"}{calls [plotspc()] to produce a spectra plot.}
 #'
 #' \item{"spcmeansd"}{plots mean spectrum +/- one standard deviation}
 #'
 #' \item{"spcprctile"}{plots 16th, 50th, and 84th percentile spectre. If the
 #' distributions of the intensities at all wavelengths were normal, this would
 #' correspond to `"spcmeansd"`. However, this is frequently not the case.
-#' Then `"spcprctile"` gives a better impression of the spectral data
-#' set.}
+#' Then `"spcprctile"` gives a better impression of the spectral data set.}
 #'
 #' \item{"spcprctl5"}{like `"spcprctile"`, but additionally the 5th and
 #' 95th percentile spectra are plotted.}
 #'
 #' \item{"map"}{calls [plotmap()] to produce a map plot.}
 #'
-#' \item{"voronoi"}{calls [plotvoronoi()] to produce a Voronoi plot
-#' (tesselated plot, like "map" for hyperSpec objects with uneven/non-rectangular
-#' grid).}
+#' \item{"voronoi"}{calls [plotvoronoi()] to produce a Voronoi plot (tesselated
+#' plot, like "map" for hyperSpec objects with uneven/non-rectangular grid).}
 #'
-#' \item{"mat"}{calls [plotmat()] to produce a plot of the spectra
-#' matrix (not to be confused with [graphics::matplot()]).}
+#' \item{"mat"}{calls [plotmat()] to produce a plot of the spectra matrix (not
+#' to be confused with [graphics::matplot()]).}
 #'
-#' \item{"c"}{calls [plotc()] to produce a calibration (or time
-#' series, depth-profile, or the like)}
+#' \item{"c"}{calls [plotc()] to produce a calibration (or time series,
+#'  depth-profile, or the like).}
 #'
-#' \item{"ts"}{plots a time series: abbrevation for `[plotc] (x,
-#' use.c = "t")`}
+#' \item{"ts"}{plots a time series: abbrevation for
+#' [`plotc(x, use.c = "t")`][`plotc()`].}
 #'
-#' \item{"depth"}{plots a depth profile: abbrevation for `[plotc]
-#' (x, use.c = "z")`} }
+#' \item{"depth"}{plots a depth profile:
+#'  abbrevation for [`plotc(x, use.c = "z")`][`plotc()`].}
+#' }
 #'
 #' @name plot-methods
 #' @rdname plot
@@ -131,11 +131,10 @@ setGeneric("plot")
 #' @param y selects what plot should be produced
 #' @param ... arguments passed to the respective plot function
 #' @author C. Beleites
-#' @seealso [plotspc()] for spectra plots (intensity over
-#'   wavelength),
+#' @seealso [plotspc()] for spectra plots (intensity over wavelength),
 #'
-#' [plotmap()] for plotting maps, i.e. color coded summary value on
-#'   two (usually spatial) dimensions.
+#' [plotmap()] for plotting maps, i.e. color coded summary value on two
+#' (usually spatial) dimensions.
 #'
 #' [plotc()]
 #'
@@ -169,5 +168,7 @@ setMethod(
 #' @export
 setMethod(
   "plot",
-  signature(x = "hyperSpec", y = "character"), .plot
+  signature(x = "hyperSpec", y = "character"),
+  .plot
 )
+
