0aea914035f0ffa2ac4c5d74b95010fbcd8a7a57
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-25 04:16:34 +0300

    Improve style

--- hyperSpec/R/spc.rubberband.R
+++ hyperSpec/R/spc.rubberband.R
@@ -22,11 +22,11 @@
 #' @note This function is still experimental
 #' @export
 #' @examples
-#' plot(paracetamol [, , 175 ~ 1800])
-#' bl <- spc.rubberband(paracetamol [, , 175 ~ 1800], noise = 300, df = 20)
+#' plot(paracetamol[, , 175 ~ 1800])
+#' bl <- spc.rubberband(paracetamol[, , 175 ~ 1800], noise = 300, df = 20)
 #' plot(bl, add = TRUE, col = 2)
 #'
-#' plot(paracetamol [, , 175 ~ 1800] - bl)
+#' plot(paracetamol[, , 175 ~ 1800] - bl)
 spc.rubberband <- function(spc, ..., upper = FALSE, noise = 0, spline = TRUE) {
   spc <- orderwl(spc)
 
@@ -44,14 +44,14 @@ spc.rubberband <- function(spc, ..., upper = FALSE, noise = 0, spline = TRUE) {
 #' @importFrom grDevices chull
 .rubberband <- function(x, y, noise, spline, ..., debuglevel = hy.getOption("debuglevel")) {
   for (s in seq_len(nrow(y))) {
-    use <- which(!is.na(y [s, ]))
+    use <- which(!is.na(y[s, ]))
 
-    pts <- chull(x [use], y [s, use])
-    pts <- use [pts]
+    pts <- chull(x[use], y[s, use])
+    pts <- use[pts]
 
     if (debuglevel >= 1L) {
-      plot(x, y [s, ], type = "l")
-      points(x [pts], y [s, pts], pch = 1, col = matlab.dark.palette(length(pts)))
+      plot(x, y[s, ], type = "l")
+      points(x[pts], y[s, pts], pch = 1, col = matlab.dark.palette(length(pts)))
     }
 
     ## `chull` returns points in cw order
@@ -60,51 +60,53 @@ spc.rubberband <- function(spc, ..., upper = FALSE, noise = 0, spline = TRUE) {
 
     ## if necessary, rotate pts so that ncol (y) is at position 1
     if (imax > 0L) {
-      pts <- c(pts [-seq_len(imax)], pts [seq_len(imax)])
+      pts <- c(pts[-seq_len(imax)], pts[seq_len(imax)])
     }
 
     ## now keep only pts until column index 1
-    pts <- pts [1:which.min(pts)]
+    pts <- pts[1:which.min(pts)]
 
     ## check whether first and last point are minima,
     ## if not remove them.
     ## If they are minima, 2nd and 2nd last point do not appear in pts
     ## last point:
-    if (pts [2] == pts [1] - 1) pts <- pts [-1]
+    if (pts[2] == pts[1] - 1) pts <- pts[-1]
 
     ## now sort ascending (anyways needed later on)
     pts <- rev(pts)
 
     ## fist point:
-    if (pts [2] == pts [1] + 1) pts <- pts [-1]
+    if (pts[2] == pts[1] + 1) pts <- pts[-1]
 
     if (debuglevel >= 1L) {
-      points(x [pts], y [s, pts], pch = 19, col = matlab.dark.palette(length(pts)), cex = 0.7)
+      points(x[pts], y[s, pts], pch = 19, col = matlab.dark.palette(length(pts)), cex = 0.7)
     }
 
-    tmp <- approx(x = x [pts], y = y [s, pts], xout = x, method = "linear")$y
+    tmp <- approx(x = x[pts], y = y[s, pts], xout = x, method = "linear")$y
 
     if (spline) {
-      pts <- which(y [s, ] <= tmp + noise)
+      pts <- which(y[s, ] <= tmp + noise)
 
       if (length(pts) > 3) {
         tmp <- predict(smooth.spline(x[pts], y[s, pts], ...)$fit, x, 0)$y
       } else {
-        tmp <- spline(x [pts], y [s, pts], xout = x)$y
+        tmp <- spline(x[pts], y[s, pts], xout = x)$y
       }
     }
 
-    y [s, ] <- tmp
+    y[s, ] <- tmp
   }
 
   y
 }
 
+# Unit tests -----------------------------------------------------------------
+
 .test(spc.rubberband) <- function() {
   context("spc.rubberband")
 
   ## use data that yields fairly stable baseline solution
-  paracetamol <- paracetamol [, , 300 ~ 550]
+  paracetamol <- paracetamol[, , 300 ~ 550]
 
   test_that("spectrum containing NA inside", {
     tmp <- paracetamol
@@ -113,7 +115,7 @@ spc.rubberband <- function(spc, ..., upper = FALSE, noise = 0, spline = TRUE) {
     coefs <- spc.rubberband(tmp)
     expect_equal(
       coefs[[, , !is.na(tmp)]],
-      spc.rubberband(paracetamol [, , !is.na(tmp)])[[]]
+      spc.rubberband(paracetamol[, , !is.na(tmp)])[[]]
     )
 
     ## bug was: all coefficients were silently 0
@@ -127,18 +129,21 @@ spc.rubberband <- function(spc, ..., upper = FALSE, noise = 0, spline = TRUE) {
     coefs <- spc.rubberband(tmp)
     expect_equal(
       coefs[[, , !is.na(tmp)]],
-      spc.rubberband(paracetamol [, , !is.na(tmp)])[[]]
+      spc.rubberband(paracetamol[, , !is.na(tmp)])[[]]
     )
   })
 
   test_that("spectrum containing NA at end", {
-    tmp <- paracetamol [1]
+    tmp <- paracetamol[1]
     tmp[[, , nwl(paracetamol), wl.index = TRUE]] <- NA
 
     coefs <- spc.rubberband(tmp)
     expect_equal(
       coefs[[, , !is.na(tmp)]],
-      spc.rubberband(paracetamol [1, , !is.na(tmp)])[[]]
+      spc.rubberband(paracetamol[1, , !is.na(tmp)])[[]]
     )
   })
+}
+
+
 }
