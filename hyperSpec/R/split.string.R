c8296d73dc5115a6087f2ce9409aa2263fb2e1e5
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-25 03:59:04 +0300

    Improve style

--- hyperSpec/R/split.string.R
+++ hyperSpec/R/split.string.R
@@ -19,11 +19,11 @@ split.string <- function(x, separator, trim.blank = TRUE, remove.empty = TRUE) {
   ncol = 2
   )
 
-  if (pos [nrow(pos), 1] > nchar(x)) {
-    pos <- pos [-nrow(pos), ]
+  if (pos[nrow(pos), 1] > nchar(x)) {
+    pos <- pos[-nrow(pos), ]
   }
 
-  x <- apply(pos, 1, function(p, x) substr(x, p [1], p [2]), x)
+  x <- apply(pos, 1, function(p, x) substr(x, p[1], p[2]), x)
 
   if (trim.blank) {
     blank.pattern <- "^[[:blank:]]*([^[:blank:]]+.*[^[:blank:]]+)[[:blank:]]*$"
@@ -31,8 +31,9 @@ split.string <- function(x, separator, trim.blank = TRUE, remove.empty = TRUE) {
   }
 
   if (remove.empty) {
-    x <- x [sapply(x, nchar) > 0]
+    x <- x[sapply(x, nchar) > 0]
   }
 
   x
 }
+
