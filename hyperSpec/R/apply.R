5c6f0bb2618323f4761625ec9d18292611180cdf
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-28 16:43:12 +0300

    Convert anon. funs into named funs in apply.R

--- hyperSpec/R/apply.R
+++ hyperSpec/R/apply.R
@@ -2,13 +2,13 @@
   if (length(unique(x)) > 1) NA else x[1]
 }
 
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 .apply <- function(data, MARGIN, FUN, ...) {
   if (length(data$spc) == 0) {
     stop("empty spectra matrix.")
   }
 
-  spc <- apply(data [, "spc", drop = FALSE], MARGIN, FUN, ...)
+  spc <- apply(data[, "spc", drop = FALSE], MARGIN, FUN, ...)
 
   if (MARGIN == 1) {
     if (is.null(spc)) {
@@ -22,7 +22,7 @@
     data$spc <- I(spc)
   } else if (MARGIN == 2) {
     if (is.null(spc)) {
-      return(data [0, ])
+      return(data[0, ])
     }
     if (is.null(dim(spc))) {
       dim(spc) <- c(1, ncol(data$spc))
@@ -36,10 +36,10 @@
       cols <- colnames(data)
       cols <- which(cols != "spc")
       if (length(cols) > 0) {
-        data [1, cols] <- lapply(data [, cols, drop = FALSE], .na.if.different)
+        data[1, cols] <- lapply(data [, cols, drop = FALSE], .na.if.different)
       }
 
-      data <- data [rep(1, nrow), , drop = FALSE]
+      data <- data[rep(1, nrow), , drop = FALSE]
 
       data$spc <- I(spc)
       rownames(data) <- rownames(spc)
@@ -49,8 +49,75 @@
   data$spc <- unclass(data$spc)
   data
 }
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.apply_hyperSpec <- function(X, MARGIN, FUN, ..., label.wl = NULL,
+  label.spc = NULL, new.wavelength = NULL, simplify) {
+  validObject(X)
 
+  if (missing(MARGIN)) { # apply for functions that the complete spectra matrix
+    ## is easier: tmp <- apply (x, , FUN, ...)
+    ## does:
+    ## tmp <- x
+    ## tmp[[]] <- FUN (x[[]], ...)
 
+    X@data$spc <- do.call(FUN, list(X@data$spc, ...))
+  } else if (all(MARGIN == 1:2)) { # apply for functions that take scalar arguments.
+
+    tmp <- apply(X@data$spc, MARGIN = MARGIN, FUN, ...)
+    tmp <- as.numeric(tmp) # otherwise surprises will be waiting
+
+    dim(tmp) <- dim(X@data$spc)
+
+    X@data$spc <- tmp
+  } else {
+    ## the usual: for each row / for each column
+
+    X@data <- .apply(X@data, MARGIN = MARGIN, FUN = FUN, ...)
+
+    if (all(MARGIN == 1)) {
+
+      ## if the number of data points per spectrum is changed, the wavelength vector needs to be
+      ## adapted, too
+
+      if (ncol(X@data$spc) != length(X@wavelength)) {
+
+        ## only internal functions here: the validation will fail until the wavelength axis is
+        ## adjusted
+
+        if (!is.null(new.wavelength)) { # vector with new wavelength is given
+          if (is.numeric(new.wavelength)) { # either directly,
+            .wl(X) <- new.wavelength
+          } else {
+            dots <- list(...)
+            .wl(X) <- dots[[new.wavelength]] # or as name of the argument that becomes the new
+            # wavelength vector
+          }
+        } else if (ncol(X@data$spc) != length(X@wavelength)) {
+          wl <- as.numeric(colnames(X@data$spc)) # if not given, try to make from colnames of the
+          # spectra matrix
+
+          if (length(wl) != ncol(X@data$spc) || any(is.na(wl))) {
+            wl <- seq_len(ncol(X@data$spc))
+          } # or just number sequentially
+
+          .wl(X) <- wl
+        }
+      }
+    }
+  }
+
+  if (!is.null(label.wl)) {
+    X@label$.wavelength <- label.wl
+  }
+
+  if (!is.null(label.spc)) {
+    X@label$spc <- label.spc
+  }
+
+  validObject(X)
+
+  X
+}
 
 #' Compute summary statistics for the spectra of a `hyperSpec` object.
 #'
@@ -116,81 +183,13 @@
 #'
 #' ## whereas MARGIN = 1 : 2 leads to FUN being called for each element separately
 #' apply(flu [, , 405:407], 1:2, print)[[]]
-setMethod("apply",
-  signature = signature(X = "hyperSpec"),
-  function(X, MARGIN, FUN, ...,
-           label.wl = NULL, label.spc = NULL, new.wavelength = NULL,
-           simplify) {
-    validObject(X)
+setMethod("apply", signature = signature(X = "hyperSpec"), .apply_hyperSpec)
 
-    if (missing(MARGIN)) { # apply for functions that the complete spectra matrix
-      ## is easier: tmp <- apply (x, , FUN, ...)
-      ## does:
-      ## tmp <- x
-      ## tmp[[]] <- FUN (x[[]], ...)
 
-      X@data$spc <- do.call(FUN, list(X@data$spc, ...))
-    } else if (all(MARGIN == 1:2)) { # apply for functions that take scalar arguments.
-
-      tmp <- apply(X@data$spc, MARGIN = MARGIN, FUN, ...)
-      tmp <- as.numeric(tmp) # otherwise surprises will be waiting
-
-      dim(tmp) <- dim(X@data$spc)
-
-      X@data$spc <- tmp
-    } else {
-      ## the usual: for each row / for each column
-
-      X@data <- .apply(X@data, MARGIN = MARGIN, FUN = FUN, ...)
-
-      if (all(MARGIN == 1)) {
-
-        ## if the number of data points per spectrum is changed, the wavelength vector needs to be
-        ## adapted, too
-
-        if (ncol(X@data$spc) != length(X@wavelength)) {
-
-          ## only internal functions here: the validation will fail until the wavelength axis is
-          ## adjusted
-
-          if (!is.null(new.wavelength)) { # vector with new wavelength is given
-            if (is.numeric(new.wavelength)) { # either directly,
-              .wl(X) <- new.wavelength
-            } else {
-              dots <- list(...)
-              .wl(X) <- dots[[new.wavelength]] # or as name of the argument that becomes the new
-              # wavelength vector
-            }
-          } else if (ncol(X@data$spc) != length(X@wavelength)) {
-            wl <- as.numeric(colnames(X@data$spc)) # if not given, try to make from colnames of the
-            # spectra matrix
-
-            if (length(wl) != ncol(X@data$spc) || any(is.na(wl))) {
-              wl <- seq_len(ncol(X@data$spc))
-            } # or just number sequentially
-
-            .wl(X) <- wl
-          }
-        }
-      }
-    }
-
-    if (!is.null(label.wl)) {
-      X@label$.wavelength <- label.wl
-    }
-
-    if (!is.null(label.spc)) {
-      X@label$spc <- label.spc
-    }
-
-    validObject(X)
-
-    X
-  }
-)
+# Unit tests -----------------------------------------------------------------
 
 #' @include unittest.R
-.test(.apply) <- function() {
+.test(.apply_hyperSpec) <- function() {
   context("apply")
 
   test_that("check whether .na.if.different is working correctly", {
