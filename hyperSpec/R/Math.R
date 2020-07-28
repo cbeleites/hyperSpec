9a18bdf52944a7bb1452b159397958019c6614a0
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-28 16:17:02 +0300

    Reorganize unit tests in Math.R

--- hyperSpec/R/Math.R
+++ hyperSpec/R/Math.R
@@ -84,26 +84,34 @@ setMethod("Math", signature(x = "hyperSpec"), .math)
 
 
 # Unit tests -----------------------------------------------------------------
-.test(abs) <- function() {
+.test(.math) <- function() {
 
-  context("log")
+  context("math")
 
   # Perform tests
-  test_that("log() works", {
+  test_that("math warnings work", {
+    expect_warning(cumsum(flu), "Do you really want to use")
+  })
+
+  test_that("math works", {
     expect_silent(flu + flu)
     expect_silent(flu ^ flu)
-    expect_silent(flu == flu)
-    expect_silent(flu >= flu | flu > flu)
-    expect_silent(all(flu == flu[[]]))
-    expect_silent(all(flu[[]] == flu))
-    expect_silent(flu[, , 445 ~ 450] > 300)
 
     expect_silent(abs(flu))
     expect_silent(sqrt(flu))
     expect_silent(round(flu, 2))
     expect_silent(max(flu))
+  })
+
+  test_that("log() works", {
     expect_silent(log(flu))
-    expect_warning(cumsum(flu), "Do you really want to use")
+  })
+
+  test_that("comparison works", {
+    expect_silent(flu == flu)
+    expect_silent(flu >= flu | flu > flu)
+    expect_silent(all(flu == flu[[]]))
+    expect_silent(all(flu[[]] == flu))
+    expect_silent(flu[, , 445 ~ 450] > 300)
   })
 }
-
