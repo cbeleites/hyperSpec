173a9fc368d02922a6897f80f520322c524e7c4a
Vilmantas Gegzna <GegznaV@gmail.com>
2020-07-28 16:23:48 +0300

    Convert anon. funs into named funs in replace.R

--- hyperSpec/R/replace.R
+++ hyperSpec/R/replace.R
@@ -1,3 +1,22 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.replace <- function(x, i, j, ..., value) {
+  validObject(x)
+
+  if (missing(i)) i <- row.seq(x)
+  if (missing(j)) j <- col.seq(x)
+
+  if (is(value, "hyperSpec")) {
+    validObject(value)
+    x@data[i, j] <- value@data
+  } else {
+    x@data[i, j] <- value
+  }
+
+  validObject(x)
+
+  x
+}
+
 #' @rdname extractreplace
 #' @name [<-
 #' @usage
@@ -21,26 +40,55 @@
 #' spc [] <- 6:1
 #' spc$..
 #' plot(spc)
-setReplaceMethod("[",
-  signature = signature(x = "hyperSpec"),
-  function(x, i, j, ..., value) {
-    validObject(x)
+setReplaceMethod("[", signature = signature(x = "hyperSpec"), .replace)
 
-    if (missing(i)) i <- row.seq(x)
-    if (missing(j)) j <- col.seq(x)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.replace2 <- function(x, i, j, l, wl.index = FALSE, ..., value) {
+  validObject(x)
 
-    if (is(value, "hyperSpec")) {
-      validObject(value)
-      x@data[i, j] <- value@data
+  if (is(value, "hyperSpec")) {
+    validObject(value)
+    value <- value@data$spc
+  }
+
+  ## check wheter a index matrix is used
+  if (!missing(i) && is.matrix(i)) {
+    if (is.logical(i)) {
+      x@data$spc[i] <- value
+    } else if (is.numeric(i) && ncol(i) == 2) {
+      if (!wl.index) {
+        i[, 2] <- .getindex(x, i[, 2], extrapolate = FALSE)
+        if (any(is.na(i[, 2]))) {
+          stop("wavelength specification outside spectral range")
+        }
+      }
+      x@data$spc[i] <- value
     } else {
-      x@data[i, j] <- value
+      stop(
+        "Index matrix i  must either be logical of the size of x$spc,",
+        "or a n by 2 matrix."
+      )
+    }
+  } else {# index by row and columns
+    if (!missing(j)) {
+      stop(
+        "The spectra matrix may only be indexed by i (spectra) and l",
+        " (wavelengths). j (data column) must be missing."
+      )
     }
 
-    validObject(x)
+    if (!missing(l) && !wl.index) {
+      l <- wl2i(x, l)
+    }
 
-    x
+    x@data$spc[i, l] <- value
   }
-)
+
+  validObject(x)
+
+  x
+}
+
 
 #' @rdname extractreplace
 #' @usage
@@ -76,55 +124,45 @@ setReplaceMethod("[",
 #' ind
 #' spc[[ind, wl.index = TRUE]] <- 9999
 #' spc[[]]
-setReplaceMethod("[[",
-  signature = signature(x = "hyperSpec"),
-  function(x, i, j, l, wl.index = FALSE, ..., value) {
-    validObject(x)
+setReplaceMethod("[[", signature = signature(x = "hyperSpec"), .replace2)
 
 
-    if (is(value, "hyperSpec")) {
-      validObject(value)
-      value <- value@data$spc
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.replace_dollar <- function(x, name, value) {
+  validObject(x)
+
+  if (is.list(value) && (length(value) == 2)) {
+    ilabel <- match("label", names(value))
+
+    if (is.na(ilabel)) {
+      ilabel <- 2
     }
 
-    ## check wheter a index matrix is used
-    if (!missing(i) && is.matrix(i)) {
-      if (is.logical(i)) {
-        x@data$spc[i] <- value
-      } else if (is.numeric(i) && ncol(i) == 2) {
-        if (!wl.index) {
-          i[, 2] <- .getindex(x, i[, 2], extrapolate = FALSE)
-          if (any(is.na(i[, 2]))) {
-            stop("wavelength specification outside spectral range")
-          }
-        }
-        x@data$spc[i] <- value
-      } else {
-        stop(
-          "Index matrix i  must either be logical of the size of x$spc,",
-          "or a n by 2 matrix."
-        )
-      }
-    } else {# index by row and columns
-      if (!missing(j)) {
-        stop(
-          "The spectra matrix may only be indexed by i (spectra) and l",
-          " (wavelengths). j (data column) must be missing."
-        )
-      }
+    label <- value[[ilabel]]
 
-      if (!missing(l) && !wl.index) {
-        l <- wl2i(x, l)
-      }
-
-      x@data$spc[i, l] <- value
-    }
-
-    validObject(x)
-
-    x
+    value <- value[[3 - ilabel]] ## the other of the 2 entries
+  } else {
+    label <- name
   }
-)
+
+  if (name == "..") { ## shortcut
+    i <- -match("spc", colnames(x@data))
+    x@data[, i] <- value
+
+    if (!is.null(label)) {
+      i <- colnames(x@data)[i]
+      i <- match(i, names(x@label))
+      x@label[i] <- label
+    }
+  } else {
+    dots <- list(x = x@data, name = name, value = value)
+    x@data <- do.call("$<-", dots)
+    x@label[[name]] <- label
+  }
+
+  x
+}
+
 
 #' @rdname extractreplace
 #' @usage
@@ -139,46 +177,11 @@ setReplaceMethod("[[",
 #' spc$z <- 1:6
 #' spc
 #' spc$z <- list(1:6, "z / a.u.")
-setReplaceMethod("$",
-  signature = signature(x = "hyperSpec"),
-  function(x, name, value) {
-    validObject(x)
+setReplaceMethod("$", signature = signature(x = "hyperSpec"), .replace_dollar)
 
-    if (is.list(value) && (length(value) == 2)) {
-      ilabel <- match("label", names(value))
-
-      if (is.na(ilabel)) {
-        ilabel <- 2
-      }
-
-      label <- value[[ilabel]]
-
-      value <- value[[3 - ilabel]] ## the other of the 2 entries
-    } else {
-      label <- name
-    }
-
-    if (name == "..") { ## shortcut
-      i <- -match("spc", colnames(x@data))
-      x@data[, i] <- value
-
-      if (!is.null(label)) {
-        i <- colnames(x@data)[i]
-        i <- match(i, names(x@label))
-        x@label[i] <- label
-      }
-    } else {
-      dots <- list(x = x@data, name = name, value = value)
-      x@data <- do.call("$<-", dots)
-      x@label[[name]] <- label
-    }
-
-    x
-  }
-)
 
 # Unit tests -----------------------------------------------------------------
-.test(replace) <- function() {
+.test(.replace) <- function() {
   ## replacement functions
   context("replace")
 
