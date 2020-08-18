dd2a3171a75ae138372cdaad0394efab58f30862
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-19 01:54:22 +0300

    Deprecate wc and count_lines
    as they are moved to hySpc.read.txt

--- hyperSpec/R/count_lines.R
+++ hyperSpec/R/count_lines.R
@@ -1,14 +1,27 @@
-#' Count Lines (of an ASCII File)
+#' Count Lines (of an ASCII File) (DEPRECATED)
+#'
+#' @description
+#'
+#' These data input related functions are **deprecated** and they will be
+#' removed in the next release of \pkg{hyperspec} package.
+#' Now functions in package \pkg{hySpc.read.txt}
+#' ([link](https://r-hyperspec.github.io/hySpc.read.txt/reference/index.html))
+#' should be used as the alternatives.
 #'
 #' @param file the file name or connection
 #' @param chunksize `file` is read in chunks of `chunksize` lines.
 #' @return number of lines in file
 #' @export
 #'
-#' @concept io
+# @concept io
+#' @concept moved to hySpc.read.txt
 #'
 #' @author C. Beleites
 count_lines <- function(file, chunksize = 1e4) {
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  deprecated_read_txt(new = "count_lines")
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   nlines <- 0
 
   con <- file(file, open = "r")
