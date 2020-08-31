5afc91774b3ae481d2ac2986f0dd7c85a1158d12
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-31 21:42:03 +0300

    Move getbynames() to read.spc.R
    As this function is only used there

--- hyperSpec/R/read.spc.R
+++ hyperSpec/R/read.spc.R
@@ -1092,3 +1092,43 @@ hySpc.testthat::test(split.string) <- function() {
   # FIXME (tests): add tests to check the correctness of the output!!!
 }
 
+
+### -----------------------------------------------------------------------------
+###
+### getbynames - get list elements by name and if no such element exists, NA
+###
+###
+
+getbynames <- function(x, e) {
+  x <- x[e]
+  if (length(x) > 0) {
+    if (is.character(e)) {
+      names(x) <- e
+    }
+    x[sapply(x, is.null)] <- NA
+    x
+  } else {
+    list()
+  }
+}
+
+
+
+# Unit tests -----------------------------------------------------------------
+hySpc.testthat::test(getbynames) <- function() {
+
+  context("getbynames")
+
+  # Perform tests
+  test_that("getbynames() works", {
+
+    lst <- list(a = 1, b = "b", c = 2i)
+
+    expect_equal(getbynames(lst, "a"), list(a = 1))
+    expect_equal(getbynames(lst, 1),   list(a = 1))
+    expect_equal(getbynames(lst, 2),   list(b = "b"))
+    expect_equal(getbynames(lst, 6)[[1]], NA)
+
+  })
+}
+
