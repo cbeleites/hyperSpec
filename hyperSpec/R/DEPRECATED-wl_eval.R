ee14e8d9954504e407bc0c90c813cc13d6b874ee
Claudia Beleites <claudia.beleites@chemometrix.gmbh>
2021-05-31 21:43:21 +0200

    fix deprecation

--- hyperSpec/R/DEPRECATED-wl_eval.R
+++ hyperSpec/R/DEPRECATED-wl_eval.R
@@ -23,6 +23,7 @@
 #' @return `hyperSpec` object containing one spectrum for each expression.
 #'
 #' @export
+#' @include wl_eval.R
 #'
 #' @seealso
 #'
@@ -32,49 +33,34 @@
 #'
 #' @author C. Beleites, V. Gegzna
 #'
-#' @examples
-#' plot(wl.eval(laser, exp = function(x) exp(-x)))
-#'
-#' plot(wl.eval(1000:4000, y = function(x) 1/log(x)))
-#'
-#' plot(wl.eval(300:550, y2 = function(x) x*2, y3 = function(x) x*3))
-#'
+
 wl.eval <- function(x, ..., normalize.wl = I) {
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   hySpc_deprecated("wl_eval")
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  UseMethod("wl.eval")
+  UseMethod("wl_eval")
 }
 
 #' @rdname DEPRECATED-wl.eval
 #' @export
 wl.eval.hyperSpec <- function(x, ..., normalize.wl = I) {
-  chk.hy(x)
-  validObject(x)
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  hySpc_deprecated("wl_eval.hyperSpec")
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  fun <- list(...)
-
-  wl <- normalize.wl(x@wavelength)
-
-  x <- decomposition(x, t(sapply(fun, function(f) f(wl))), scores = FALSE)
-  x$.f <- if (is.null(names(fun))) {
-    rep(NA, length(fun))
-  } else {
-    names(fun)
-  }
-  x
+  wl_eval.hyperSpec (x, ..., normalize.wl = I)
 }
 
 
 #' @rdname DEPRECATED-wl.eval
 #' @export
 wl.eval.numeric <- function(x, ..., normalize.wl = I) {
-  if (!is.vector(x)) {
-    class_txt <- paste(class(x), collapse = ", ")
-    stop("`x` must be a vector. Now it is ", class_txt, ".")
-  }
-  x <- new("hyperSpec", spc = seq_along(x), wavelength = x)
-  wl.eval(x, ..., normalize.wl = normalize.wl)
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  hySpc_deprecated("wl_eval.numeric")
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  wl_eval.numeric (x, ..., normalize.wl = I)
+
 }
 
 
@@ -84,84 +70,11 @@ wl.eval.numeric <- function(x, ..., normalize.wl = I) {
 hySpc.testthat::test(wl.eval.hyperSpec) <- function() {
   context("wl.eval")
 
-  test_that("error on function not returning same length as input", {
-    expect_error(wl.eval(flu, function(x) 1))
+  test_that("deprecated", {
+    expect_warning(wl.eval.hyperSpec (flu, function(x) x),
+                   "Function 'wl.eval.hyperSpec' is deprecated.")
+    expect_warning(wl.eval.numeric (1:5, function(x) x),
+                   "Function 'wl.eval.numeric' is deprecated.")
   })
 
-  test_that("wl.eval(<hyperSpec>) against manual evaluation", {
-    expect_equivalent(
-      wl.eval(flu, function(x) rep(5, length(x)), normalize.wl = normalize01)[[]],
-      matrix(rep(5, nwl(flu)), nrow = 1)
-    )
-
-    expect_equivalent(
-      wl.eval(flu, function(x) x),
-      vanderMonde(flu, 1)[2]
-    )
-
-    expect_equivalent(
-      wl.eval(flu, function(x) exp(-x))[[]],
-      matrix(exp(-flu@wavelength), nrow = 1)
-    )
-  })
-
-  test_that("normalization", {
-    expect_equivalent(
-      wl.eval(flu, function(x) rep(5, length(x)), normalize.wl = normalize01)[[]],
-      matrix(rep(5, nwl(flu)), nrow = 1)
-    )
-
-    expect_equivalent(
-      wl.eval(flu, function(x) x, normalize.wl = normalize01)[[]],
-      matrix(seq(0, 1, length.out = nwl(flu)), nrow = 1)
-    )
-
-    expect_equivalent(
-      wl.eval(flu, function(x) exp(x), normalize.wl = normalize01)[[]],
-      matrix(exp(seq(0, 1, length.out = nwl(flu))), nrow = 1)
-    )
-  })
-
-
-  test_that("multiple functions", {
-    expect_equivalent(
-      wl.eval(flu, function(x) rep(1, length(x)), function(x) x),
-      vanderMonde(flu, 1)
-    )
-  })
-
-  test_that("function names", {
-    tmp <- wl.eval(flu, f = function(x) x, g = function(x) exp(-x))
-
-    expect_equal(tmp$.f, c("f", "g"))
-  })
-
-  test_that("wl.eval(<numeric>) works", {
-
-    expect_equal(
-      as.vector(wl.eval(1:10, f = function(x) x)$spc),
-      1:10
-    )
-
-    expect_equal(
-      as.vector(wl.eval(1:10, f = function(x) x**2)$spc),
-      (1:10)**2
-    )
-
-    expect_equal(
-      wl.eval(wl(flu), f = function(x) x)$.f,
-      wl.eval(   flu,  f = function(x) x)$.f
-    )
-
-    expect_warning(
-      tmp <- wl.eval(300:500, f = function(x) x, g = function(x) exp(-x)),
-      "deprecated"
-    )
-    expect_equal(tmp$.f, c("f", "g"))
-  })
-
-  test_that("wl.eval fails with matrix input", {
-    expect_error(wl.eval(matrix(1:10), f = function(x) x))
-    expect_error(wl.eval(matrix(), f = function(x) x))
-  })
 }
