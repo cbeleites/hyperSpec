e1c72bde3943ea53df577a034f774d985789f44b
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-10 16:42:26 +0300

    Update function style

--- hyperSpec/R/hy_unittest.R
+++ hyperSpec/R/hy_unittest.R
@@ -1,5 +1,8 @@
 #' @export
 
 skip_if_not_fileio_available <- function() {
-  testthat::skip_if_not(file.exists("fileio"), message = "file import test files not installed")
+  testthat::skip_if_not(
+    file.exists("fileio"),
+    message = "file import test files not installed"
+  )
 }
