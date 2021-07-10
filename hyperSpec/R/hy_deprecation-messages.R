01de9609272da303387f1eacc6c2126a3a2d1093
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-10 23:18:39 +0300

    Update hy_deprecation-messages.R

--- hyperSpec/R/hy_deprecation-messages.R
+++ hyperSpec/R/hy_deprecation-messages.R
@@ -45,12 +45,28 @@ deprecated_ggplot2 <- function(new = "", old = as.character(sys.call(sys.parent(
   hySpc_deprecated(new = new, package = "hySpc.ggplot2", old = old)
 }
 
-deprecated_read_txt <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
-  hySpc_deprecated(new = new, package = "hySpc.read.txt", old = old)
+deprecated_read_envi <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
+  hySpc_deprecated(new = new, package = "hySpc.read.ENVI", old = old)
+}
+
+deprecated_read_jdx <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
+  hySpc_deprecated(new = new, package = "hySpc.read.jdx", old = old)
 }
 
 deprecated_read_mat <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
   hySpc_deprecated(new = new, package = "hySpc.read.mat", old = old)
+}
+
+deprecated_read_spc <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
+  hySpc_deprecated(new = new, package = "hySpc.read.spc", old = old)
+}
+
+deprecated_read_spe <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
+  hySpc_deprecated(new = new, package = "hySpc.read.spe", old = old)
+}
+
+deprecated_read_txt <- function(new = "", old = as.character(sys.call(sys.parent()))[1L]) {
+  hySpc_deprecated(new = new, package = "hySpc.read.txt", old = old)
 }
 
 # suppress_warnings() is created to overcome issue that suppressWarnings()
@@ -72,9 +88,15 @@ hySpc.testthat::test(deprecated_ggplot2) <- function() {
   context("Deprecation messages")
 
   test_that("Deprecation message is a warning", {
-    expect_warning(hySpc_deprecated(), "is deprecated")
-    expect_warning(deprecated_ggplot2(), "is deprecated")
-    expect_warning(deprecated_ggplot2(), "ggplot2")
+    expect_warning(hySpc_deprecated(),      "is deprecated")
+    expect_warning(deprecated_ggplot2(),    "is deprecated")
+    expect_warning(deprecated_ggplot2(),    "ggplot2")
     expect_warning(deprecated_ggplot2("a"), "ggplot2")
+    expect_warning(deprecated_read_envi(),  "ENVI")
+    expect_warning(deprecated_read_jdx(),   "jdx")
+    expect_warning(deprecated_read_spc(),   "spc")
+    expect_warning(deprecated_read_spe(),   "spe")
+    expect_warning(deprecated_read_mat(),   "mat")
+    expect_warning(deprecated_read_txt(),   "txt")
   })
 }
