b84aa2b4630c73b002e3e4fbdca2aa854997cf4f
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-13 02:49:40 +0300

    mean_sd: anonymous to hidden fun, update style

--- hyperSpec/R/dim.R
+++ hyperSpec/R/dim.R
@@ -1,3 +1,9 @@
+# Function -------------------------------------------------------------------
+.ncol <- function(x) {
+  validObject(x)
+  ncol(x@data)
+}
+
 #' Number of rows (spectra), columns, and data points per spectrum of a
 #' `hyperSpec` object
 #'
@@ -12,46 +18,54 @@
 #'
 #' @rdname dim
 #' @docType methods
-#' @param x a `hyperSpec` object
-#' @author C. Beleites
-#' @seealso [base::ncol()]
 #'
-#' @return `nrow()`, `ncol()`, `nwl()`, and `length()`, return an `integer`.
+#' @param x a `hyperSpec` object
+#'
+#' @return `nrow()`, `ncol()`, `nwl()`, and `length()` return an `integer`.
+#'
+#' @author C. Beleites
+#'
+#' @concept summary
+#' @seealso [base::ncol()]
 #'
 #' @export
 #'
-#' @concept summary
-#'
 #' @examples
 #' ncol(faux_cell)
-setMethod("ncol", signature = signature("hyperSpec"), function(x) {
+setMethod("ncol", signature = signature("hyperSpec"), .ncol)
+
+
+# Function -------------------------------------------------------------------
+
+.nrow <- function(x) {
   validObject(x)
 
-  ncol(x@data)
-})
+  nrow(x@data)
+}
 
+#' @rdname dim
+#'
 #' @details
 #' - `nrow()` yields the number of rows in `x@@data`, i.e. the number of
 #'   spectra in the `hyperSpec` object.
 #'
-#' @rdname dim
 #' @seealso [base::nrow()]
 #' @export
 #' @examples
 #'
 #' nrow(faux_cell)
-setMethod("nrow", signature = signature("hyperSpec"), function(x) {
-  validObject(x)
+setMethod("nrow", signature = signature("hyperSpec"), .nrow)
 
-  nrow(x@data)
-})
 
+# Function -------------------------------------------------------------------
+
+#' @rdname dim
+#' @aliases nwl
+#'
 #' @details
 #' - `nwl()` returns the number of columns in `x@@data$spc`, i.e. thelength of
 #'   each spectrum.
 #'
-#' @rdname dim
-#' @aliases nwl
 #' @export
 #' @examples
 #'
@@ -64,35 +78,49 @@ nwl <- function(x) {
 }
 
 
+# Function -------------------------------------------------------------------
 
+.dim <- function(x) {
+  validObject(x)
+  c(nrow = nrow(x@data), ncol = ncol(x@data), nwl = ncol(x@data$spc))
+}
+
+#' @rdname dim
 #' @details
 #' - `dim()` returns all three values in a vector.
 #'
-#' @rdname dim
 #' @return
 #'
 #' `dim()` returns a vector of length 3.
+#'
 #' @seealso [base::dim()]
 #' @keywords methods
+#'
 #' @export
+#'
 #' @examples
 #'
 #' dim(faux_cell)
-setMethod("dim", signature = signature("hyperSpec"), function(x) {
-  validObject(x)
-  c(nrow = nrow(x@data), ncol = ncol(x@data), nwl = ncol(x@data$spc))
-})
+setMethod("dim", signature = signature("hyperSpec"), .dim)
 
+
+# Function -------------------------------------------------------------------
+
+.length <- function(x) {
+  validObject(x)
+  nrow(x@data)
+}
+
+#' @rdname dim
+#'
 #' @details
 #' - `length()` is a synonym for `nrow()`. It is supplied so that `seq_along(x)`
 #' returns a sequence to index each spectrum.
-#' @rdname dim
 #' @seealso [base::length()]
+#'
 #' @export
+#'
 #' @examples
 #'
 #' length(faux_cell)
-setMethod("length", signature = signature("hyperSpec"), function(x) {
-  validObject(x)
-  nrow(x@data)
-})
+setMethod("length", signature = signature("hyperSpec"), .length)
--- hyperSpec/R/dimnames.R
+++ hyperSpec/R/dimnames.R
@@ -1,32 +1,38 @@
-#' Dimnames for `hyperSpec` objects
-#'
-#' `hyperSpec` objects can have row- and column names like data.frames.
-#' The "names" of the wavelengths are treated separately: see [wl()].
-#'
-#' @param x the `hyperSpec` object
-#' @aliases dimnames
-#' @rdname dimnames
-#' @docType methods
-#' @author C. Beleites
-#' @seealso [wl()] for the wavelength dimension
-#'
-#' [base::dimnames()]
-#'
-#' @export
-#'
-#' @keywords methods
-#' @concept manipulation
-#'
-#' @examples
-#' dimnames(flu)
-setMethod("dimnames", signature = signature(x = "hyperSpec"), function(x) {
+# Function -------------------------------------------------------------------
+
+.dimnames <- function(x) {
   validObject(x)
 
   list(
     row = rownames(x@data), data = colnames(x@data),
     wl = colnames(x@data$spc)
   )
-})
+}
+
+#' Dimnames for `hyperSpec` objects
+#'
+#' `hyperSpec` objects can have row- and column names like data.frames.
+#' The "names" of the wavelengths are treated separately: see [wl()].
+#'
+#' @param x the `hyperSpec` object
+#'
+#' @rdname dimnames
+#' @aliases dimnames
+#' @docType methods
+#'
+#' @author C. Beleites
+#'
+#' @keywords methods
+#' @concept manipulation
+#' @seealso [wl()] for the wavelength dimension
+#'
+#' [base::dimnames()]
+#'
+#' @export
+#'
+#' @examples
+#' dimnames(flu)
+setMethod("dimnames", signature = signature(x = "hyperSpec"), .dimnames)
 
 #' @rdname dimnames
 #' @aliases rownames
@@ -46,58 +52,83 @@ setMethod("rownames", signature = signature(x = "hyperSpec"), function(x, do.NUL
   rownames(x@data, do.NULL = do.NULL, prefix = prefix)
 })
 
-#' @param value the new names
-#' @usage
-#' \S4method{rownames}{hyperSpec} (x) <- value
-#' @aliases rownames<-,hyperSpec-method
-#' @rdname dimnames
-#' @name rownames<-
-#' @export "rownames<-"
-#'
-#' @concept manipulation
-#'
-setReplaceMethod("rownames", signature = signature(x = "hyperSpec"), function(x, value) {
+
+# Function -------------------------------------------------------------------
+
+.rownames_replace <- function(x, value) {
   validObject(x)
 
   rownames(x@data) <- value
   x
-})
+}
+
+#' @name rownames<-
+#' @rdname dimnames
+#' @aliases rownames<-,hyperSpec-method
+#'
+#' @param value the new names
+#' @usage
+#' \S4method{rownames}{hyperSpec} (x) <- value
+#'
+#' @export "rownames<-"
+#'
+#' @concept manipulation
+#'
+setReplaceMethod("rownames",
+  signature = signature(x = "hyperSpec"),
+  .rownames_replace
+)
+
+
+# Function -------------------------------------------------------------------
+
+.colnames <- function(x, do.NULL = TRUE, prefix = "col") {
+  validObject(x)
+  colnames(x@data, do.NULL = do.NULL, prefix = prefix)
+}
 
 #' @rdname dimnames
 #' @aliases colnames
 #' @seealso [base::colnames()]
-#' @export
 #'
 #' @concept manipulation
 #'
+#' @export
+#'
 #' @examples
 #' colnames(faux_cell)
-setMethod("colnames",
-  signature = signature(x = "hyperSpec"),
-  function(x, do.NULL = TRUE, prefix = "col") {
-    validObject(x)
-    colnames(x@data, do.NULL = do.NULL, prefix = prefix)
-  }
-)
+setMethod("colnames", signature = signature(x = "hyperSpec"), .colnames)
 
+
+# Function -------------------------------------------------------------------
+
+.colnames_replace <- function(x, value) {
+  validObject(x)
+
+  names(x@label[colnames(x@data)]) <- value
+  colnames(x@data) <- value
+
+  validObject(x) # necessary: $spc could be renamed!
+  x
+}
+
+#' @name colnames<-
 #' @rdname dimnames
+#' @aliases colnames<-,hyperSpec-method
+#'
 #' @usage
 #' \S4method{colnames}{hyperSpec} (x) <- value
-#' @aliases colnames<-,hyperSpec-method
-#' @name colnames<-
+#'
 #' @export "colnames<-"
 #'
 #' @concept manipulation
 #'
 setReplaceMethod("colnames",
   signature = signature(x = "hyperSpec"),
-  function(x, value) {
-    validObject(x)
-
-    names(x@label[colnames(x@data)]) <- value
-    colnames(x@data) <- value
-
-    validObject(x) # necessary: $spc could be renamed!
-    x
-  }
+  .colnames_replace
 )
+
+
+# Unit tests -----------------------------------------------------------------
+
+# TODO: add unit tests
