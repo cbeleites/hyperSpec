fe29a24307921abb31b5d1e200720623b32a579a
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-14 14:10:04 +0300

    Compare: anonymous to hidden fun, update style

--- hyperSpec/R/Compare.R
+++ hyperSpec/R/Compare.R
@@ -1,7 +1,18 @@
+# Function -------------------------------------------------------------------
+
+.Compare_hh <-  function(e1, e2) {
+    validObject(e1)
+    validObject(e2)
+
+    callGeneric(e1[[]], e2[[]])
+  }
+
+
 #' @title Comparison of `hyperSpec` objects
 #'
 #' @description
-#' The comparison operators `>`, `<`, `>=`, `<=`, `==`, and `!=` for `hyperSpec` objects.
+#' The comparison operators `>`, `<`, `>=`, `<=`, `==`, and `!=` for `hyperSpec`
+#' objects.
 #'
 #' `all.equal` checks the equality of two `hyperSpec` objects.
 #'
@@ -15,18 +26,31 @@
 #' With numeric vectors [hyperSpec::sweep()] might be more
 #' appropriate.
 #'
-#' If you want to calculate on the `data.frame` `hyperSpec@@data`,
-#' you have to do this directly on `hyperSpec@@data`.
+#' If you want to calculate on the `data.frame` `hyperSpec@data`,
+#' you have to do this directly on `hyperSpec@data`.
 #'
-#' @author C. Beleites
 #' @name Comparison
 #' @rdname Comparison
+#' @aliases Comparison Operators
+#' Comparison,hyperSpec
+#' comparison,hyperSpec
+#' Operators,hyperSpec
+#' operators,hyperSpec
+#' compare,hyperSpec
+#' Compare,hyperSpec-method
+#' Compare,hyperSpec,hyperSpec-method
+#' Compare,hyperSpec,matrix-method
+#' Compare,hyperSpec,numeric-method
+#' Compare,matrix,hyperSpec-method
+#' Compare,numeric,hyperSpec-method
+#' <,hyperSpec,hyperSpec-method
+#' >,hyperSpec,hyperSpec-method
+#' <=,hyperSpec,hyperSpec-method
+#' >=,hyperSpec,hyperSpec-method
+#' ==,hyperSpec,hyperSpec-method
+#' !=,hyperSpec,hyperSpec-method
 #' @docType methods
-#' @aliases Comparison Operators Compare,hyperSpec-method
-#' Compare,hyperSpec,hyperSpec-method <,hyperSpec,hyperSpec-method >,hyperSpec,hyperSpec-method
-#' <=,hyperSpec,hyperSpec-method >=,hyperSpec,hyperSpec-method ==,hyperSpec,hyperSpec-method
-#' !=,hyperSpec,hyperSpec-method Compare,hyperSpec,matrix-method Compare,hyperSpec,numeric-method
-#' Compare,matrix,hyperSpec-method Compare,numeric,hyperSpec-method
+#'
 #' @param e1,e2 Either two `hyperSpec` objects or one `hyperSpec`
 #'   object and matrix of same size as `hyperSpec[[]]` or a scalar
 #'   (numeric of length 1).
@@ -45,36 +69,27 @@
 #'   [hyperSpec::Math()] for mathematical group generic functions
 #'   (groups Math and Math2) working on `hyperSpec` objects.
 #'
-#' @export
+#' @author C. Beleites
 #'
 #' @keywords methods arith
 #' @concept manipulation
+#'
+#' @export
 #'
 #' @examples
 #'
 #' flu[, , 445 ~ 450] > 300
 #'
 #' all(flu == flu[[]])
-setMethod(
-  "Compare", signature(e1 = "hyperSpec", e2 = "hyperSpec"),
-  function(e1, e2) {
-    validObject(e1)
-    validObject(e2)
+setMethod("Compare", signature(e1 = "hyperSpec", e2 = "hyperSpec"), .Compare_hh)
 
-    callGeneric(e1[[]], e2[[]])
-  }
-)
+
+# Function -------------------------------------------------------------------
 
 .compx <- function(e1, e2) {
   validObject(e1)
   callGeneric(e1[[]], e2)
 }
-
-.compy <- function(e1, e2) {
-  validObject(e2)
-  callGeneric(e1, e2[[]])
-}
-
 
 #' @rdname Comparison
 setMethod("Compare", signature(e1 = "hyperSpec", e2 = "numeric"), .compx)
@@ -83,8 +98,21 @@ setMethod("Compare", signature(e1 = "hyperSpec", e2 = "numeric"), .compx)
 setMethod("Compare", signature(e1 = "hyperSpec", e2 = "matrix"), .compx)
 
 
+# Function -------------------------------------------------------------------
+
+.compy <- function(e1, e2) {
+  validObject(e2)
+  callGeneric(e1, e2[[]])
+}
+
 #' @rdname Comparison
 setMethod("Compare", signature(e1 = "numeric", e2 = "hyperSpec"), .compy)
 
 #' @rdname Comparison
 setMethod("Compare", signature(e1 = "matrix", e2 = "hyperSpec"), .compy)
+
+
+# Unit tests -----------------------------------------------------------------
+
+# TODO: add unit tests
+
