3dfab6b6ceae9dd52c2f3cf5a83a15754d717c60
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-08-11 13:09:47 +0300

    Export more "future" functions
    See https://github.com/cbeleites/hyperSpec/pull/346#issuecomment-892700872

--- hyperSpec/R/NEW-functions.R
+++ hyperSpec/R/NEW-functions.R
@@ -27,3 +27,52 @@ NULL
 wl_convert_units <- function(x, from, to, ref_wl = NULL) {
   wlconv(points = x, src = from, dst = to, laser = ref_wl)
 }
+
+#' @rdname Future-functions
+#' @include options.R
+#' @export
+hy_set_options <- function(...) {
+  hy.setOption(...)
+}
+
+#' @rdname Future-functions
+#' @include options.R
+#' @export
+hy_get_option <- function(...) {
+  hy.getOption(...)
+}
+
+#' @rdname Future-functions
+#' @include options.R
+#' @export
+hy_set_options <- function(...) {
+  hy.getOptions(...)
+}
+
+#' @rdname Future-functions
+#' @include read.txt.long.R
+#' @export
+read_txt_long <- function(...) {
+  read.txt.long(...)
+}
+
+#' @rdname Future-functions
+#' @include read.txt.wide.R
+#' @export
+read_txt_wide <- function(...) {
+  read.txt.wide(...)
+}
+
+#' @rdname Future-functions
+#' @include wl.R
+#' @export
+.wl_fix_unit_name <- function(...) {
+  .fixunitname(...)
+}
+
+#' @rdname Future-functions
+#' @include chk.hy.R
+#' @export
+assert_hyperSpec  <- function(...) {
+  chk.hy(...)
+}
